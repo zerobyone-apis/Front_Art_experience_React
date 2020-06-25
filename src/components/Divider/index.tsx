1cdca8c43186a85aef4d8b83f5084a666a65b7a0
damian <damianezetiel1@gmail.com>
2020-06-25 03:13:11 -0300

    fixes in divider

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -4,10 +4,11 @@ import { url } from 'inspector';
 
 export const Divider = (props: {
     img: string,
-    align: string
+    align: string,
+    id?: string,
 }) => {
     return (
-        <div className="divider-box">
+        <div id={props.id} className="divider-box">
             <div style={{ backgroundImage: props.img }} className={`divider_${props.align}`}>
                 {/* <img className="img_paralax" src={props.img} alt="" /> */}
             </div>
