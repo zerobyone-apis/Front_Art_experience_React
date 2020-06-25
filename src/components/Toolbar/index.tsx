ff7e076365421eacf5728420718fef62b79b7d14
damian <damianezetiel1@gmail.com>
2020-06-25 03:13:34 -0300

    fixes in toolbar

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { AccountMenu } from '../AccountMenu';
 import { ReservationModal } from '../ReservationModal';
+import { Button } from '../Button';
 import './Toolbar.scss';
 
 export const Toolbar = () => {
     return (
         <div className="toolbar">
-            <p className="title">ArtExperience</p>
+            <div id="start_page" />
+            <div className="left-box">
+                <p className="title">ArtExperience</p>
+                <Button href="#about_us" className="toolbar-btn" label="Acerca de Nosotros" />
+                <Button href="#services" className="toolbar-btn" label="Servicios" />
+                <Button href="#courses" className="toolbar-btn" label="Cursos" />
+                <Button href="#contact" className="toolbar-btn" label="Contacto" />
+            </div>
             <div className="right-box">
-                <ReservationModal />
+                <ReservationModal className="reservationModal" />
                 {/* <AccountMenu /> */}
             </div>
         </div>
