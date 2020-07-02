4403bfa1cc54f635f75d9cd69ad27fd1b41cde1a
damian <damianezetiel1@gmail.com>
2020-07-01 22:26:32 -0300

    fixes in Toolbar

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -3,17 +3,23 @@ import { AccountMenu } from '../AccountMenu';
 import { ReservationModal } from '../ReservationModal';
 import { Button } from '../Button';
 import './Toolbar.scss';
+import '../../styles/ArtExperienceButtons.scss';
+import '../../styles/ArtExperienceFonts.scss';
+import { LeftMenu } from '../LeftMenu';
+import { toolbarButtons } from '../../utils/toolbarButtons';
 
 export const Toolbar = () => {
     return (
         <div className="toolbar">
             <div id="start_page" />
             <div className="left-box">
-                <p className="title">ArtExperience</p>
-                <Button href="#about_us" className="toolbar-btn" label="Acerca de Nosotros" />
-                <Button href="#services" className="toolbar-btn" label="Servicios" />
-                <Button href="#courses" className="toolbar-btn" label="Cursos" />
-                <Button href="#contact" className="toolbar-btn" label="Contacto" />
+                <LeftMenu />
+                <p className="art_experience-title title">ArtExperience</p>
+                {
+                    toolbarButtons.map((button, i) => {
+                        return <Button key={i} href={button.href} className="art_experience-button toolbar-btn" label={button.label} />
+                    })
+                }
             </div>
             <div className="right-box">
                 <ReservationModal className="reservationModal" />
