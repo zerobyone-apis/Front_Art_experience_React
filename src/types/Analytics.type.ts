5bd2c587d139a8bcc83e8737a1bddce230b4259c
damian <damianezetiel1@gmail.com>
2020-07-02 03:14:43 -0300

    add types

---
+++ src/types/Analytics.type.ts
@@ -0,0 +1,5 @@
+export interface IAnalitycs {
+  startDate: string;
+  endDate: string;
+  result?: string;
+}
---
+++ src/types/Enterprise.type.ts
@@ -0,0 +1,16 @@
+export interface IEnterprise {
+  id?: number;
+  createddate?: string;
+  enterprisename: string;
+  phone: number;
+  cellphone: number;
+  email: string;
+  fax?: number;
+  location: string;
+  enterpriserules: string;
+  firstmessage?: string;
+  secondmessage?: string;
+  urllogo: string;
+  lastupdate: string;
+  username?: string;
+}
