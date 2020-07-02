0ed3ce7f25958698efc9b3d991bdbf3dd1979e47
damian <damianezetiel1@gmail.com>
2020-07-01 22:27:02 -0300

    add toolbarButtons file in utils

---
+++ src/utils/toolbarButtons.ts
@@ -0,0 +1,9 @@
+/**
+ * Used in Toolbar & LeftMenu
+ */
+export const toolbarButtons = [
+    { href: "#about_us", label: 'Acerca de Nosotros' },
+    { href: "#services", label: 'Servicios' },
+    { href: "#courses", label: 'Cursos' },
+    { href: "#contact", label: 'Contacto' }
+];
