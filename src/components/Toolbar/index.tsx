8c45a299ab5cbd30d2e9b1fa029aea3a876fbefa
damian <damianezetiel1@gmail.com>
2020-07-09 17:08:37 -0300

    fix in Toolbar

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -14,7 +14,7 @@ export const Toolbar = () => {
             <div className="left-box">
                 <LeftMenu />
                 {/* <img src={logo} alt="" /> */}
-                <img className="logo-img" src="https://raw.githubusercontent.com/zerobyone-apis/Front_Art_experience_React/master/src/assets/logo_gold.png" alt="" />
+                <img className="logo-img" src="https://raw.githubusercontent.com/zerobyone-apis/Front_Art_experience_React/master/src/assets/gold_logo.png" alt="" />
                 {/* <Button
                     href="#"
                     labelClassName="art_experience-title"
