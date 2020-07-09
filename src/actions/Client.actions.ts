d42458cd01abf2c8548b32bb8dea65f24b0a0e0a
damian <damianezetiel1@gmail.com>
2020-07-09 03:37:04 -0300

    add ClientActions

---
+++ src/actions/Client.actions.ts
@@ -0,0 +1,26 @@
+import moment from 'moment';
+import IntegrationBackend from '../utils/IntegrationBackend';
+import { IClient } from '../types/Client.type'
+import { GET_ENDPOIT, CLIENT_ROUTE } from '../types/Routes.type';
+export default class ClientActions {
+    private backend: IntegrationBackend = new IntegrationBackend();
+
+    get = async (email: string) => {
+        try {
+            let data = { email };
+            let responseClient: IClient = await this.backend.send(
+                GET_ENDPOIT,
+                data,
+                CLIENT_ROUTE
+            );
+            return responseClient;
+        } catch (error) {
+            console.error(`Error: getAll barber-> ${error.message}`);
+            return null;
+        }
+    }
+
+    add = async (newClient: IClient) => {
+
+    }
+}
