82add3c51b25081458bf52f26999dd7a7024654a
damian <damianezetiel1@gmail.com>
2020-07-09 03:39:32 -0300

    add ReserveFooter component

---
+++ src/components/ReserveModal/ReserveFooter/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button } from '../../Button';
+import './ReserveFooter.scss';
+
+export const ReserveFooter = (props: {
+    wizard: number,
+    onChangeWizard: any,
+    checkStep: any
+}) => {
+    return <div className="footer">
+        <div className="footer_right-box">
+            {
+                props.wizard ? (
+                    <Button
+                        className="footer-button"
+                        label="Volver"
+                        onClick={() => {
+                            props.onChangeWizard(props.wizard - 1);
+                        }}
+                    />
+                ) : null
+            }
+            {
+                !props.checkStep() ? null : (
+                    <Button
+                        className="art_experience-button footer-button confirm"
+                        label={props.wizard < 4 ? 'Siguiente' : 'Reservar'}
+                        onClick={() => {
+                            if (props.wizard < 4) {
+                                props.onChangeWizard(props.wizard + 1);
+                            } else {
+                                // createReserve();
+                            }
+                        }}
+                    />
+                )
+            }
+        </div>
+    </div>
+}
