99d78f2080b4708361984bc573d716b3336c5a70
damian <damianezetiel1@gmail.com>
2020-07-09 03:41:38 -0300

    fixes in Divider component

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { ReactChildren, Children, ReactChild } from 'react';
 import './Divider.scss';
 import { url } from 'inspector';
 
 export const Divider = (props: {
+    children?: ReactChild,
     img: string,
     align: string,
     id?: string,
 }) => {
     return (
         <div id={props.id} className="divider-box">
-            <div style={{ backgroundImage: props.img }} className={`divider_${props.align}`}>
-                {/* <img className="img_paralax" src={props.img} alt="" /> */}
+            <div className={`divider_${props.align}`}>
+                <div className="back_img" style={{ backgroundImage: props.img }} />
+                <div className="divider_items">
+                    {props.children}
+                </div>
             </div>
         </div>
     )
