79dd936fd7d378778aea8e068320dc190f915da4
damian <damianezetiel1@gmail.com>
2020-07-09 03:40:28 -0300

    add ServicesList component

---
+++ src/components/ReserveModal/ServicesList/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './ServicesList.scss';
+
+export const ServicesList = (props: {
+    value: any,
+    setService: any,
+    services: any[]
+}) => {
+    return (
+        <div className="services-box">
+            <div className="list_services-box">
+                {props.services.map((service, i) => {
+                    return (
+                        <div
+                            className={`service ${props.value.name === service.name ? 'selected-service' : null}`}
+                            onClick={() => {
+                                props.setService(service);
+                            }}
+                            key={i}
+                        >
+                            <div className="name">
+                                <p>
+                                    {service.name}
+                                </p>
+                            </div>
+                            <div className="price">
+                                <p>
+                                    ${service.cost}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
