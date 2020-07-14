ec7cecd5e2aaf482df38692df27415249789f7f6
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-07-14 20:09:17 -0300

    Remove CyberManager

--- src/hooks/useOutsideAlerter.tsx
+++ src/hooks/useOutsideAlerter.tsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
-/**
-* Hook that alerts clicks outside of the passed ref
-*/
 
-export const useOutsideAlerter = (ref, showMenu) => {
+// Hook that alerts clicks outside of the passed ref
+
+
+export const useOutsideAlerter = (ref:any, showMenu:any) => {
     useEffect(() => {
-        /**
-         * Alert if clicked on outside of element
-         */
-        function handleClickOutside(event) {
+        
+         // Alert if clicked on outside of element
+         
+        function handleClickOutside(event:any) {
             if (ref.current && !ref.current.contains(event.target)) {
                 showMenu(false);
             }
--- src/types/Analytics.type.ts
+++
@@ -1,5 +0,0 @@
-export interface IAnalitycs {
-  startDate: string;
-  endDate: string;
-  result?: string;
-}
