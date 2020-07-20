f705a6e37086049d29e3696c50f0239917256235
damian <damianezetiel1@gmail.com>
2020-07-20 01:53:08 -0300

    add context in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React, { ReactElement, FC } from 'react';
+import { ButtonProvider } from '../contexts/ButtonsContext';
+import { ClientProvider } from '../contexts/ClientContext';
 import './index.scss';
 const App: FC<{ Component: any; pageProps: any }> = ({
   Component,
   pageProps,
-}) => <Component {...pageProps} />;
+}) => {
+  return (
+    <div>
+      <ButtonProvider disabled={false}>
+        <ClientProvider >
+          <Component {...pageProps} />
+        </ClientProvider>
+      </ButtonProvider>
+    </div>
+  )
+};
 export default App;
