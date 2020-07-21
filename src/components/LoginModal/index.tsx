0261cd48e8ebf5209a161e4b7157f0d679d2a76f
damian <damianezetiel1@gmail.com>
2020-07-21 04:28:29 -0300

    fix structure and add props LoginModal

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -1,11 +1,15 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, SetStateAction, Dispatch } from 'react';
 import { DialogModal } from '../DialogModal';
 import { ClientAccess } from '../ReserveModal/ClientAccess';
 import { IClient } from '../../types/Client.type';
 import { Button } from '../Button';
 import './LoginModal.scss';
 
-export const LoginModal = () => {
+export const LoginModal = (props: {
+    show?: boolean,
+    onClose?: Dispatch<SetStateAction<boolean>>,
+    onSuccessLogin: any
+}) => {
     const defaultClient: IClient = {
         cel: '',
         email: '',
@@ -15,26 +19,37 @@ export const LoginModal = () => {
         userId: -1
     }
 
-    const [showDialog, setShowDialog] = useState(false);
+    const [showDialog, setShowDialog] = useState(props.show || false);
     const [client, setClient] = useState(defaultClient);
+
+    const onClientLogged = (clientData: any) => {
+        setClient(clientData);
+        props.onSuccessLogin(true);
+    }
 
     return (
         <div className="login-modal">
-            <div className="dialog_activator-box">
-                <Button
-                    onClick={() => { setShowDialog(true) }}
-                    className={`activator-btn login-btn art_experience-button_outlined`}
-                    label={'Acceder'} />
-            </div>
+            {!props.show ? (
+                <div className="dialog_activator-box">
+                    <Button
+                        onClick={() => { setShowDialog(true) }}
+                        className={`activator-btn login-btn art_experience-button_outlined`}
+                        label={'Acceder'} />
+                </div>
+            ) : null}
+
             {!showDialog ? null : (
                 <DialogModal
                     title="Inicio de Sesion - ArtExperience"
                     className="login-dialog"
                     width='65vw'
                     height='65vh'
-                    onClose={() => { setShowDialog(false) }}
+                    onClose={() => {
+                        setShowDialog(false);
+                        props.onClose(false);
+                    }}
                 >
-                    <ClientAccess onClientLogged={setClient} />
+                    <ClientAccess onClientLogged={onClientLogged} />
                 </DialogModal>
             )
             }
