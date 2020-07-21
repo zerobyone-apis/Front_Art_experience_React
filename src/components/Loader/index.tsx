6f0472f328f7963fa913b8eeae390f8fa30199e5
damian <damianezetiel1@gmail.com>
2020-07-21 20:51:04 -0300

    fix style of Loader Component

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Loader.scss';
+import { FaCut } from 'react-icons/fa'
 
 export const Loader = () => {
     return (
         <div className="loader">
-            <div className="lds-dual-ring" />
+
+            <div className="lds-ellipsis">
+                {/* <p className="title">Cargando</p> */}
+                <FaCut className="icon" />
+                <FaCut className="icon" />
+                <FaCut className="icon" />
+                <FaCut className="icon" />
+            </div>
         </div>
     )
 }
