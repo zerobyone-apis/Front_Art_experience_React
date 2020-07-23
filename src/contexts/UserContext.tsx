ee4811656c20a35968f8e4a503b02108a7e34500
damian <damianezetiel1@gmail.com>
2020-07-23 04:36:41 -0300

    add UserContext

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/display-name */
+import React, {
+    createContext,
+    useState,
+    ReactElement,
+    useEffect,
+} from 'react';
+import { IUser } from '../types/User.type';
+import { USER_DATA_STORAGE } from '../types/StorageData.type';
+
+export const UserContext = createContext({
+    userIsLogged: () => undefined,
+    getUserData: () => undefined,
+    setUserData: (userData: any) => undefined,
+});
+
+export const UserProvider = (props: {
+    value?: IUser,
+    children: ReactElement;
+}) => {
+    let storex = require('store'); // store :3
+
+    const [user, setUser] = useState(props.value || null);
+
+    useEffect(() => {
+        storex.set(USER_DATA_STORAGE, user);
+    }, [user])
+
+    const setUserData = (userData: any) => {
+        setUser(userData);
+    }
+    const getUserData = () => {
+        return user;
+    }
+    const userIsLogged = () => {
+        return user ? true : false;
+    }
+    const context = { userIsLogged, getUserData, setUserData };
+    return (
+        <UserContext.Provider value={context}>
+            {props.children}
+        </UserContext.Provider>
+    );
+};
