2a8fceb76544ca0ff40df28b12621ea9e91632e1
damian <damianezetiel1@gmail.com>
2020-07-23 04:36:17 -0300

    add Dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Table } from '../components/Dashboard/Table';
+import ReserveActions from '../actions/Reserve.actions';
+import { ButtonContext } from '../contexts/ButtonsContext';
+import { Toolbar } from '../components/Dashboard/Toolbar';
+import { LoaderPage } from '../components/LoaderPage';
+import './Dashboard.scss';
+
+const DashboardPage = () => {
+    const reserveActions: ReserveActions = new ReserveActions();
+    const [reserves, setReserve] = useState([]);
+    // context
+    const {
+        // @ts-ignore
+        disabled,
+        setDisabledButton
+    } = useContext(ButtonContext);
+
+    const headerOrder = [
+        { text: 'ID', value: 'reserveId' },
+        { text: 'Cliente', value: 'nameClient' },
+        { text: 'Fecha de Reservacion', value: 'startTime' },
+        { text: 'Servicio', value: 'workToDo' },
+    ];
+
+    useEffect(() => {
+        getReserves();
+    }, [])
+
+    const getReserves = async () => {
+        setDisabledButton(true)
+        const reserves = await reserveActions.getAll();
+        setDisabledButton(false)
+        if (reserves) {
+            setReserve(reserves)
+        }
+    }
+
+    return (
+        <div className="dashboard_page">
+            <Toolbar />
+            <div className="page-box">
+                <div className="dashboard">
+                    {reserves.length ?
+                        (<Table items={reserves} headers={headerOrder} />) : null}
+                </div>
+            </div>
+            <LoaderPage show={disabled} />
+        </div>
+    );
+}
+
+DashboardPage.displayName = 'Dashboard Page';
+export default DashboardPage;
