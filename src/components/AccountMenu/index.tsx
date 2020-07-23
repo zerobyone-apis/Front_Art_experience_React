725d206bf2aeff4d2096e45a350fb7be8486a237
damian <damianezetiel1@gmail.com>
2020-07-23 04:41:29 -0300

    fix AccountMenu

--- src/components/AccountMenu/index.tsx
+++ src/components/AccountMenu/index.tsx
@@ -18,7 +18,8 @@ export const AccountMenu = () => {
           className="user-icon"
           src="https://lh3.googleusercontent.com/-WMtgXup2p9s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuckFuVbksvkez3bMA9T2wIluX33o_w/photo.jpg?sz=46" alt="" />
       </div>
-      <div ref={wrapperRef} className="user-menu" style={{ visibility: (showMenu ? "visible" : "hidden") }}>
+
+      <div ref={wrapperRef} className="user-menu">
         <p className="item-text user-name">Damian Rodriguez</p>
         <p className="item-text user-email">damianezetiel1@gmail.com</p>
         <hr style={{ borderColor: '#FFFFFF' }} />
