fb3c50e494300d92cf03e2b6ab27670a664f8736
damian <damianezetiel1@gmail.com>
2020-07-24 05:16:35 -0300

    fix in CustomTable

--- src/components/Dashboard/CustomTable/index.tsx
+++ src/components/Dashboard/CustomTable/index.tsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
+import { Button } from '../../Button';
+import { SearchField } from '../../SearchField';
 import { Grid, Paper } from '@material-ui/core';
 import './Table.scss';
+import '../../../styles/ArtExperienceButtons.scss';
 export const CustomTable = (props: {
     items: any[],
-    headers: { text: string, value: string }[]
+    headers: { text: string, value: string }[],
+    noItemsMessage?: string,
+    noSearchMessage?: string,
+    showSearchField?: boolean,
 }) => {
-    const [items, setItems] = useState(props.items || []);
+    const [filtredItems, setFiltredItems] = useState(props.items || []);
     const useStyles = makeStyles((theme: Theme) =>
         createStyles({
             root: {
@@ -21,60 +26,76 @@ export const CustomTable = (props: {
         }),
     );
     const classes = useStyles();
-
-    // useEffect(()=>{
-    //     items.unshift(headers)
-    // },[])
-
     return (
-        <div className="table-box">
-            <div className="no-orders">
-                {/* <p>No tiene ordenes creadas</p> */}
+        <div className="custom-table">
+            <div>
+                <SearchField
+                    items={filtredItems}
+                    onChangeResults={setFiltredItems}
+                    itemFilter="nameClient"
+                    fieldLabel="Buscar"
+                />
             </div>
-            <div className="no-orders">
-                {/* <p>No se encontraron coincidencias</p> */}
-            </div>
-
+            {!filtredItems && props.noItemsMessage ?
+                (
+                    <div className="no-orders">
+                        <p>{props.noItemsMessage}</p>
+                    </div>
+                ) : null
+            }
+            {!filtredItems && props.noSearchMessage ?
+                (
+                    <div className="no-orders">
+                        <p>{props.noSearchMessage}</p>
+                    </div>
+                ) : null
+            }
             <div className="table">
-                {items.map((row: any, rowIndex) => {
+                {filtredItems.map((row: any, rowIndex) => {
                     return (
-                        <div className="order" key={rowIndex}>
+                        <div className="table-box" key={rowIndex}>
                             <div className="left-box">
                             </div>
                             <div className="content-box">
                                 <div className={classes.root}>
                                     {/* HEADER */}
-                                    <Grid container xs={12} xl={12} sm={12} spacing={3}>
-                                        {
-                                            props.headers.map((header: { text: string, value: string }, headerIndex) => {
-                                                return (
-                                                    <Grid item xs={2} xl={2} sm={2} spacing={2}>
-                                                        <p
-                                                            className="item_table-text"
-                                                        >{header.text}</p>
-                                                    </Grid>
-                                                )
-                                            })
-                                        }
-                                    </Grid>
+                                    {rowIndex === 0 ? (
+                                        <div className="header">
+                                            <Grid container xl={12} spacing={3}>
+                                                {
+                                                    props.headers.map((header: { text: string, value: string }, headerIndex) => {
+                                                        return (
+                                                            <Grid item xs={2} xl={2} sm={2} key={headerIndex}>
+                                                                <Button className="art_experience-button_only-text" label={header.text} />
+                                                            </Grid>
+                                                        )
+                                                    })
+                                                }
+                                            </Grid>
+                                        </div>
+                                    ) : null}
                                     {/* ROWS */}
-                                    <Grid container xs={12} xl={12} sm={12} spacing={3}>
-                                        {
-                                            props.headers.map((header: { text: string, value: string }, headerIndex) => {
-                                                return (
-                                                    <Grid item xs={2} xl={2} sm={2} spacing={2}>
-                                                        {
-                                                            header.value != 'status' ? (
-                                                                <p
-                                                                    className="item_table-text"
-                                                                >{row[header.value]}</p>
-                                                            ) : null
-                                                        }
-                                                    </Grid>
-                                                )
-                                            })
-                                        }
-                                    </Grid>
+                                    <div className="row">
+                                        <Grid container xl={12} spacing={3}>
+                                            {
+                                                props.headers.map((header: { text: string, value: string }, headerIndex) => {
+                                                    return (
+                                                        <Grid item xs={2} xl={2} sm={2} key={headerIndex}>
+                                                            {
+                                                                header.value != 'status' ? (
+                                                                    <p
+                                                                        className="item_table-text"
+                                                                    >{row[header.value]}</p>
+                                                                ) : (
+                                                                        <Button className="art_experience-button_outlined" label="status" />
+                                                                    )
+                                                            }
+                                                        </Grid>
+                                                    )
+                                                })
+                                            }
+                                        </Grid>
+                                    </div>
                                 </div>
                             </div>
                             <div className="right-box">
