9ecd3ec39d449db7b9f0a7e65d58ec2f55afe0b9
damian <damianezetiel1@gmail.com>
2020-07-27 23:29:31 -0300

    fix in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState, useContext, ChangeEvent } from 'react';
+import React, { useContext } from 'react';
 import { ButtonContext } from '../../contexts/ButtonsContext';
 
 import './Button.scss';
@@ -7,6 +7,7 @@ import '../../styles/ArtExperienceFonts.scss';
 export const Button = (props: {
   label?: string;
   onClick?: any;
+  formRef?: string;
   width?: string;
   children?: React.ReactChild;
   color?: string;
@@ -17,18 +18,15 @@ export const Button = (props: {
   href?: string;
   type?: 'button' | 'submit' | 'reset';
 }) => {
-
+  // CONTEXTS
   const {
     // @ts-ignore
     disabled
   } = useContext(ButtonContext);
-
   return (
     <div className={`${props.className} ${disabled ? 'disabled' : ''} button-holder`}>
-
       <button
-        disabled={disabled}
-        // disabled={props.disabled}
+        disabled={props.disabled || disabled}
         style={{
           'width': props.width,
           background: props.color,
@@ -40,7 +38,6 @@ export const Button = (props: {
         <a href={props.href}>
           <div>
             {props.children}
-
             {!props.label ? null :
               <p className={`${props.labelClassName} label_button art_experience-text-light`}
                 style={{ marginLeft: (props.children ? "10px" : "0px") }}>
