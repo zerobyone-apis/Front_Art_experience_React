1529400056f58958e83a9adeba9b321912469e47
damian <damianezetiel1@gmail.com>
2020-07-27 23:29:22 -0300

    fix in user action

--- src/actions/User.actions.ts
+++ src/actions/User.actions.ts
@@ -1,5 +1,6 @@
 import IntegrationBackend from '../utils/IntegrationBackend';
-import { GET_ENDPOIT, USER_SIGN_IN_ROUTE } from '../types/Routes.type';
+import { GET_ENDPOIT, USER_SIGN_IN_ROUTE, USER_ROUTE, POST_ENDPOIT } from '../types/Routes.type';
+import { IUser } from '../types/User.type';
 export default class UserActions {
   private backend: IntegrationBackend = new IntegrationBackend();
 
@@ -14,12 +15,42 @@ export default class UserActions {
         data,
         USER_SIGN_IN_ROUTE
       );
-
       console.log(response)
       return response.data;
     } catch (error) {
       console.log('Error xd')
       return null
     }
+  }
+
+  register = async (user: IUser) => {
+    try {
+      const data: IUser = {
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        cel: user.cel,
+        admin: user.admin,
+        barberId: user.barberId,
+        createOn: user.createOn,
+        fullName: user.fullName,
+        status: user.status,
+        userId: user.userId
+      };
+      const response: any = await this.backend.send(
+        POST_ENDPOIT,
+        data,
+        USER_ROUTE
+      );
+      if (response.status !== 201) {
+        console.log('Error on create client', response.message)
+        return Error('Error on create client')
+      }
+      console.log('success post client')
+      return response.data;
+    } catch (error) {
+      return "Ocurrio un error! Vuelva a intentarlo"
+    }
+
   }
 }
