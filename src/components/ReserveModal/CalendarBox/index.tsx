b6e0efb1b7530a3195364a79ae8425f643e3453b
damian <damianezetiel1@gmail.com>
2020-07-29 21:54:44 -0300

    fix f*****g problem with the css of Calendar

--- src/components/ReserveModal/CalendarBox/index.tsx
+++ src/components/ReserveModal/CalendarBox/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 import './CalendarBox.scss';
 
 export const CalendarBox = (props: {
