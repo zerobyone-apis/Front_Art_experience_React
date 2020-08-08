f272a13cdf90f0fe98b0cddd1df8c561094044bf
damian <damianezetiel1@gmail.com>
2020-08-08 03:50:13 -0300

    fix in toolbar

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { LoginModal } from '../LoginModal';
-import { ReserveModal } from '../ReserveModal';
-import { Button } from '../Button';
-import { LeftMenu } from '../LeftMenu';
-import { toolbarButtons } from '../../utils/toolbarButtons';
 import './Toolbar.scss';
 import '../../styles/ArtExperienceButtons.scss';
 import '../../styles/ArtExperienceFonts.scss';
 
+import { Button } from '../Button';
+import { LeftMenu } from '../LeftMenu';
+import { LoginModal } from '../LoginModal';
+import React from 'react';
+import { ReserveModal } from '../ReserveModal';
+import { toolbarButtons } from '../../utils/toolbarButtons';
 
 export const Toolbar = () => {
     return (
@@ -20,17 +20,16 @@ export const Toolbar = () => {
                         className="logo-img"
                         src="https://raw.githubusercontent.com/zerobyone-apis/Front_Art_experience_React/master/src/assets/gold_logo.png" alt="" />
                 </a>
-                {
-                    toolbarButtons.map((button, i) => {
-                        return (
-                            <Button
-                                key={i}
-                                href={button.href}
-                                className="art_experience-button_only-text toolbar-btn"
-                                label={button.label}
-                            />
-                        )
-                    })
+                {toolbarButtons.map((button, i) => {
+                    return (
+                        <Button
+                            key={i}
+                            href={button.href}
+                            className="art_experience-button_only-text toolbar-btn"
+                            label={button.label}
+                        />
+                    )
+                })
                 }
             </div>
             <div className="right-box">
