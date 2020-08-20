6274f0fbc6a4e69344a1136474f187aec5a2e32c
damian <damianezetiel1@gmail.com>
2020-08-19 23:12:07 -0300

    fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { ButtonProvider } from '../contexts/ButtonsContext';
+import React, { FC, useContext } from 'react';
+import { ButtonProvider, ButtonContext } from '../contexts/ButtonsContext';
 import { BARBERLIST_DATA_STORAGE } from '../types/StorageData.type';
 import { BarberListProvider } from '../contexts/BarberListContext';
 import { USER_DATA_STORAGE } from '../types/StorageData.type';
@@ -8,15 +8,20 @@ import { ThemeProvider } from '../contexts/ThemeContext';
 import { THEME_DARK, THEME_LIGHT } from '../types/Themes.type';
 import '../styles/theme.scss';
 import './app.scss';
+import { LoaderPage } from '../components/LoaderPage';
 
 const App: FC<{ Component: any; pageProps: any }> = ({
   Component,
   pageProps,
 }) => {
+  const {
+    // @ts-ignore
+    disabled
+  } = useContext(ButtonContext);
   const store = require('store'); // store :3
   return (
     <div>
-      <ButtonProvider disabled={false}>
+      <ButtonProvider disabled={true}>
         <UserProvider value={store.get(USER_DATA_STORAGE) || null}>
           <BarberListProvider value={store.get(BARBERLIST_DATA_STORAGE) || null}>
             <ThemeProvider value={THEME_DARK}>
