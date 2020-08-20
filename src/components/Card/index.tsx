bfdc415b5d4d1496f1fe864a3efaf64f449915fe
damian <damianezetiel1@gmail.com>
2020-08-19 23:09:01 -0300

    fixes in Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -21,9 +21,9 @@ export const Card = (props: {
     return (
         <div className={`${props.className + ' ' || ''}card ${props.theme ? props.theme : getTheme()}`} id={props.id}>
             <div className="anchor" id="about_us"></div>
-            <p className={`text-${props.theme ? props.theme : getTheme()} card_title art-title title`}
+            <p className={`text-${props.theme ? props.theme : getTheme()} card_title title`}
             >{props.title}</p>
-            <p className={`card_subtitle art-text text-${props.theme ? props.theme : getTheme()}`}
+            <p className={`card_subtitle text text-${props.theme ? props.theme : getTheme()}`}
             >{props.subtitle}</p>
             <div className="card_items">
                 {props.children}
