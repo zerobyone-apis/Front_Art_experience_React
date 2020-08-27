4b14ea76eadcbb7c740c1eb1d673bfd13cd21e8e
damian <damianezetiel1@gmail.com>
2020-08-26 23:38:55 -0300

    integrate firebaseContext and fix style hours-box

--- src/components/reserve-modal/reserve-time/reserve-time.tsx
+++ src/components/reserve-modal/reserve-time/reserve-time.tsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Button } from '../../button/button';
-import AvailableTimeActions from '../../../actions/AvailableTime.actions';
 import { CalendarBox } from '../calendar-box/calendar-box';
-import { HourBox } from '../hour-box/hour-box';
-import moment from 'moment';
 import { ThemeContext } from '../../../contexts/ThemeContext';
+import { FirebaseContext } from '../../../contexts/FirebaseContext';
+import AvailableTimeActions from '../../../actions/AvailableTime.actions';
+import moment from 'moment';
 import './reserve-time.scss';
 import '../../../styles/effects.scss';
 import '../../../styles/theme-buttons.scss';
-import db from '../../../config/firebase';
 
 export const ReserveTime = (props: {
-  reserveDate: Date;
-  reserveHour: string;
-  barberId: number;
-  onSelctDate: any;
-  onSelctHour: any;
+  reserveDate: Date,
+  reserveHour: string,
+  barberId: number,
+  onSelctDate: any,
+  onSelctHour: any,
 }) => {
-  var flag = false;
   // list of all reserves: [ date: string, hours: string[] ]
   const [reservesList, setReservesList] = useState([]);
   // hours in HoursBox
@@ -27,115 +25,81 @@ export const ReserveTime = (props: {
   const [reserveDate, setReserveDate] = useState(undefined);
   const [reserveHour, setReserveHour] = useState(props.reserveHour || null);
 
-  //? States firebase
-  const [reservesDatesFirebase, setReservesDatesFirebase] = useState([]);
-  const [reservesHoursFirebase, setReservesHoursFirebase] = useState([]);
-  const [firebaseObj, setFirebaseObj] = useState([]);
-
   const {
     // @ts-ignore
     getTheme,
   } = useContext(ThemeContext);
+  const {
+    // @ts-ignore
+    getData,
+  } = useContext(FirebaseContext);
 
   const timeActions: AvailableTimeActions = new AvailableTimeActions();
 
-  //This is not necessary anymore
   const getHoursByBarberShop = async () => {
     let response = await timeActions.getBarberShopTime();
-    return response;
-  };
+    return response
+  }
 
-  //? From firebase datahook Realtime
-  const getHoursByBarberShopUsingFirebase = (barberName) => {
-    // How we are having the realtime database we can use
-    // this method directly to check the db under the hook 'useEffect()'
-    let response = timeActions.getDatesByReservesFirebase(barberName);
-    console.log('Objeto response', response);
-    setFirebaseObj(response);
-    setBarberShopTime(response);
-
-
-    
-    console.log('doctimes > ', ((reserve: { date: string; times: string[] }) => reserve.date.valueOf()))
-    
-    
-    response.map(doc => console.log('docdate > ',doc.date))
-    //Set the list of hours from firebase
-    
-    //TODO: Estos hooks aun no se setean con los valores
-    //TODO: hay que Averiguar como lograr setearlos para 
-    //TODO: poder setear el estado de horas y mostrar los componentes  
-    setReservesHoursFirebase(
-      response.map((reserve: { date: string; times: string[] }) => {
-        console.log('this is the doc date ', reserve.date);
-        console.log('this is the reserve time ', reserve.times);
-
-        let times = reserve.times;
-        if (times) {
-          console.log(
-            'this is the times ->',
-            times
-          );
-          return times;
-        }
-      })
-    );
-
-    //Set the list of dates from firebase
-    setReservesDatesFirebase(
-      response.map((reserve: { date: string; times: string[] }) => {
-        console.log('this is the doc date ', reserve.date);
-        console.log('this is the reserve time ', reserve.times);
-
-        let date = reserve.date;
-        if (date) {
-          console.log(
-            'this is the date ->',
-            new Date(date).toUTCString()
-          );
-          return new Date(date).toUTCString();
-        }
-      })
-    );
-
-
-    console.log('response firebase Obj -> ', firebaseObj);
-    console.log('response barberShopTime -> ', barberShopTime);
-    console.log('response reserves dates -> ', reservesDatesFirebase);
-    console.log('response reserves hours-> ', reservesHoursFirebase);
-  };
-
-  //This is not necessary anymore
   const getDatesByReserves = async () => {
     let response = await timeActions.getDatesByReserves(props.barberId);
     return response;
-  };
+  }
 
-  //useEffect firebase
   useEffect(() => {
-    //? Suponiendo como funca aca deberias tener un arreglo de objetos y se lo seteas
-    //? a barberShopTime
-    //? getHoursByBarberShop().then((response: any) => { setBarberShopTime(response) })
-    getHoursByBarberShopUsingFirebase('mariano.moreno');
-    getDatesByReserves()
-      .then((response: any) => {
-        setReservesList(response);
-      })
-      .then((result) => {
+    getHoursByBarberShop().then((response: any) => { setBarberShopTime(response) })
+    /* in this case not found firebase functions
+      getDatesByReserves().then((response: any) => {
+      setReservesList(response);
+    }).then(
+      result => {
         setReserveDate(props.reserveDate);
-      });
-  }, []);
+      })
+    */
+    // test this for execute useEffect and charge dates and hours
+    setReserveDate(props.reserveDate);
+  }, [])
 
   useEffect(() => {
+    // firebase data
+    let data: {
+      date: { seconds: number, nanoseconds: number },
+      times: string[]
+    }[] = getData();
+
+    console.log('get data: ', data)
+
+    /* Formating date for use in component
+      { date: { seconds: 3434954, nanoseconds: 0 } }
+      to
+      { date: '2020-08-26', hours: ['16:00'] },
+    */
+
+    let formattedDates: { date: string, hours: string[] }[] = [];
+
+    data.map((item) => {
+      let date: any = item.date;
+      formattedDates.push({
+        date: moment(new Date(date.toDate())
+          .toUTCString())
+          .format()
+          .toString()
+          .split('T')[0],
+        hours: item.times
+      })
+    });
+
+    setReservesList(formattedDates);
+    console.log('data para reserve-time: ', formattedDates)
     if (reserveDate) {
       let filterHours = onSelectDate(reserveDate);
-      setAvailableHours(filterHours);
+      setAvailableHours(filterHours)
     }
-  }, [reserveDate]);
+  }, [reserveDate])
 
   useEffect(() => {
-    setListHours(availableHours);
-  }, [availableHours]);
+    setListHours(availableHours)
+  }, [availableHours])
 
   const onSelectDate = (selectedDate: Date) => {
     let formatSelectedDate = moment(selectedDate).format('YYYY-MM-DD');
@@ -145,104 +109,65 @@ export const ReserveTime = (props: {
     props.onSelctDate(selectedDate);
     let listBusyHours = [];
     // get busy hours by reserve date
-    reservesList.map((reserve: { date: string; hours: string[] }) => {
+    reservesList.map((reserve: { date: string, hours: string[] }) => {
       if (reserve.date === formatSelectedDate) {
         reserve.hours.map((reserveHour: string) => {
-          listBusyHours.push(reserveHour.substr(0, 5));
+          listBusyHours.push(reserveHour.substr(0, 5))
         });
       }
-    });
+    })
     // filter available hours by barberShop hours
     let availables = [];
     let actualHour = moment(new Date(), 'HH:mm:ss');
     let actualDate = moment(new Date()).format('YYYY-MM-DD');
-    barberShopTime.map((barberShopHour) => {
+    barberShopTime.map(barberShopHour => {
       if (listBusyHours.indexOf(barberShopHour) === -1) {
         // check if is after barbershop hour to actual hour if is the actual date
         if (actualDate === formatSelectedDate) {
           let formatBarberShopHour = moment(`${barberShopHour}:00`, 'HH:mm:ss');
           if (formatBarberShopHour.isAfter(actualHour)) {
-            availables.push(barberShopHour);
+            availables.push(barberShopHour)
           }
         } else {
-          availables.push(barberShopHour);
+          availables.push(barberShopHour)
         }
       }
-    });
+    })
     return availables;
-  };
-
-  const onSelectDateUsingFirebase = (selectedDate: Date) => {
-    let formatSelectedDate = moment(selectedDate).format('YYYY-MM-DD');
-    setReserveDate(selectedDate);
-    setReserveHour(undefined);
-    props.onSelctHour(undefined);
-    props.onSelctDate(selectedDate);
-    let listBusyHours = [];
-    // get busy hours by reserve date
-    firebaseObj.map((reserve: { date: string; times: string[] }) => {
-      if (reserve.date === formatSelectedDate) {
-        reserve.times.map((reserveHour: string) => {
-          console.log('Time format -> ', reserveHour);
-          listBusyHours.push(reserveHour);
-        });
-      }
-    });
-
-    // filter available hours by barberShop hours
-    let availables = [];
-    let actualHour = moment(new Date(), 'HH:mm:ss');
-    let actualDate = moment(new Date()).format('YYYY-MM-DD');
-    firebaseObj.map((reserve: { date: string; times: string[] }) => {
-      if (listBusyHours.indexOf(reserve.times) === -1) {
-        // check if is after barbershop hour to actual hour if is the actual date
-        if (actualDate === formatSelectedDate) {
-          let formatBarberShopHour = moment(`${reserve.times}:00`, 'HH:mm:ss');
-          if (formatTimes.isAfter(actualHour)) {
-            availables.push(reserve.times);
-          }
-        } else {
-          availables.push(barberShopHour);
-        }
-      }
-    });
-    return availables;
-  };
+  }
 
   const onSelectHour = (selectedHour: string) => {
     setReserveHour(selectedHour);
     props.onSelctHour(selectedHour);
-  };
+  }
 
   return (
     <div className="time-box effect-slide_top">
-      {/* <p>{`hora: ${reserveHour}`}</p>
-            <p>{`fecha: ${reserveDate}`}</p> */}
-      <CalendarBox value={reserveDate} onSelectDate={onSelectDate} />
+
+      <CalendarBox
+        value={reserveDate}
+        onSelectDate={onSelectDate} />
+
       {availableHours.length ? (
-        <div className="hours-item">
-          <div className="hours-box effect-slide_top">
-            {availableHours.map((hour, i) => (
-              <Button
-                className={`theme-button-outlined 
-                                 hour-item 
-                                ${
-                                  reserveHour === hour ? 'selected-hour' : null
-                                }`}
-                key={i}
-                label={hour}
-                onClick={() => {
-                  onSelectHour(hour);
-                }}
-              />
-            ))}
+        <div className="hours-box effect-slide_top">
+          <div className="container">
+            {availableHours.map((hour, i) =>
+              <div className={`hour-item ${reserveHour === hour ? 'selected-hour' : null}`}>
+                <Button
+                  className={`theme-button-outlined`}
+                  key={i}
+                  label={hour}
+                  onClick={() => {
+                    onSelectHour(hour)
+                  }} />
+              </div>
+            )}
           </div>
         </div>
       ) : (
-        <p className={`no-hours text text-${getTheme()}`}>
-          No hay horarios disponibles para esta fecha
-        </p>
-      )}
-    </div>
+          <p className={`no-hours text text-${getTheme()}`}>No hay horarios disponibles para esta fecha</p>
+        )
+      }
+    </div >
   );
-};
+}
