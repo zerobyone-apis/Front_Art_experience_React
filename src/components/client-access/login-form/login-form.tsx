36d39756fbbb9d710d07be8b6530e695dc1bd056
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-08-29 05:29:04 -0300

    The POST Method was sending the userID what wrong ID for the Endpoint

--- src/components/client-access/login-form/login-form.tsx
+++ src/components/client-access/login-form/login-form.tsx
@@ -9,89 +9,93 @@ import './login-form.scss';
 import '../../../styles/theme-buttons.scss';
 
 export const LoginForm = (props: {
-    onClientLogged: (response: any) => void
+  onClientLogged: (response: any) => void;
 }) => {
-    const defaultLoginFields = {
-        email: '',
-        password: '',
+  const defaultLoginFields = {
+    email: '',
+    password: '',
+  };
+
+  const baseMessage = {
+    value: '',
+    isError: false,
+  };
+
+  const [loginFields, setLoginFields] = useState(defaultLoginFields);
+  const [message, setMessage] = useState(baseMessage);
+
+  const onChangeLoginField = (value: string, fieldName: string) => {
+    setLoginFields({ ...loginFields, [fieldName]: value });
+  };
+
+  const clientActions: ClientActions = new ClientActions();
+
+  const {
+    // @ts-ignore
+    disabled,
+    setDisabledButton,
+  } = useContext(ButtonContext);
+  const {
+    // @ts-ignore
+    setUserData,
+  } = useContext(UserContext);
+
+  // LOGIN
+  const login = async () => {
+    setMessage({ value: '', isError: false }); //clear
+    const fields = {
+      email: loginFields.email,
+      password: loginFields.password,
     };
-
-    const baseMessage = {
-        value: '',
-        isError: false
+    setDisabledButton(true);
+    const response = await clientActions.login(fields);
+    if (response) {
+      props.onClientLogged(response);
+      setUserData({ ...response.data.user, ...response.data.client });
+      setMessage({ value: 'Has iniciado sesion con exito', isError: false });
+    } else {
+      setMessage({
+        value: 'No se pudo iniciar sesion, verifique email y contaseña',
+        isError: true,
+      });
     }
+    setDisabledButton(false);
+  };
 
-    const [loginFields, setLoginFields] = useState(defaultLoginFields);
-    const [message, setMessage] = useState(baseMessage)
-
-    const onChangeLoginField = (value: string, fieldName: string) => {
-        setLoginFields({ ...loginFields, [fieldName]: value })
-    }
-
-    const clientActions: ClientActions = new ClientActions();
-
-    const {
-        // @ts-ignore
-        disabled,
-        setDisabledButton
-    } = useContext(ButtonContext);
-    const {
-        // @ts-ignore
-        setUserData
-    } = useContext(UserContext);
-
-    // LOGIN
-    const login = async () => {
-        setMessage({ value: '', isError: false }); //clear
-        const fields = {
-            email: loginFields.email,
-            password: loginFields.password
-        }
-        setDisabledButton(true);
-        const response = await clientActions.login(fields);
-        if (response) {
-            props.onClientLogged(response);
-            setUserData({ ...response.data.user });
-            setMessage({ value: 'Has iniciado sesion con exito', isError: false });
-        } else {
-            setMessage({ value: 'No se pudo iniciar sesion, verifique email y contaseña', isError: true });
-        }
-        setDisabledButton(false);
-    }
-
-    return (
-        <div className="login-box">
-            <ValidationForm
-                objectTest={loginFields}
-                buttonLabel="Acceder"
-                buttonClassName="access_btn theme-button-outlined"
-                onClick={() => {
-                    login()
-                }}
-            >
-                <TextField
-                    value={loginFields.email}
-                    name="email"
-                    type="email"
-                    required={true}
-                    label="Email"
-                    className="theme-text_field--dark"
-                    onChange={onChangeLoginField} />
-                <TextField
-                    value={loginFields.password}
-                    name="password"
-                    type="password"
-                    required={true}
-                    label="Contraseña"
-                    className="theme-text_field--dark"
-                    onChange={onChangeLoginField} />
-            </ValidationForm>
-            {message.isError ? (
-                <p className="error_message">{message.value}</p>
-            ) : (
-                    <p className="success_message">{message.value}</p>
-                )
-            }
-        </div>
-    )
-}
+  return (
+    <div className="login-box">
+      <ValidationForm
+        objectTest={loginFields}
+        buttonLabel="Acceder"
+        buttonClassName="access_btn theme-button-outlined"
+        onClick={() => {
+          login();
+        }}
+      >
+        <TextField
+          value={loginFields.email}
+          name="email"
+          type="email"
+          required={true}
+          label="Email"
+          className="theme-text_field--dark"
+          onChange={onChangeLoginField}
+        />
+        <TextField
+          value={loginFields.password}
+          name="password"
+          type="password"
+          required={true}
+          label="Contraseña"
+          className="theme-text_field--dark"
+          onChange={onChangeLoginField}
+        />
+      </ValidationForm>
+      {message.isError ? (
+        <p className="error_message">{message.value}</p>
+      ) : (
+        <p className="success_message">{message.value}</p>
+      )}
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ const IndexPage = () => {
   const {
     // @ts-ignore
     setDisabledButton,
-    disabled
+    disabled,
   } = useContext(ButtonContext);
   const {
     // @ts-ignore
@@ -42,7 +42,7 @@ const IndexPage = () => {
 
   useEffect(() => {
     setDisabledButton(true);
-    getBarbers().then(response => {
+    getBarbers().then((response) => {
       setBarbers(response);
       setBarberList(response); // save in store
       setDisabledButton(false);
@@ -51,19 +51,19 @@ const IndexPage = () => {
     // getPageInfo();
     // getServices();
     // getCourses();
-  }, [])
+  }, []);
 
   const pageInfo = {
-    name: "Art Experience",
-    slogan: "Space for Men",
-    openTimeLV: "11 a 19hs",
-    openTimeSBD: "10 a 18hs",
-    number: "22913056",
-    cel: "095 499 023",
-    email: "art.experience.uy.2020@gmail.com",
+    name: 'Art Experience',
+    slogan: 'Space for Men',
+    openTimeLV: '11 a 19hs',
+    openTimeSBD: '10 a 18hs',
+    number: '22913056',
+    cel: '095 499 023',
+    email: 'art.experience.uy.2020@gmail.com',
     instagram: 'https://www.instagram.com/artexperiencee/',
     facebook: 'https://www.facebook.com/artexperiencee/?epa=SEARCH_BOX',
-    aboutUsTitle: "Acerca de Nosotros",
+    aboutUsTitle: 'Acerca de Nosotros',
     aboutUs: `
       En Art Experience nos enfocamos en brindar un servicio excepcional con la finalidad de que nuestros clientes se sientan comodos y agustos \n
       en un ambiente muy divertido y profesional en el cual se valora el tiempo de cada uno de nuestros clientes\n 
@@ -71,41 +71,39 @@ const IndexPage = () => {
       sabemos muy bien que la barberia es una zona de relajacion donde a muchos de nuestros clientes les gusta venir a relajarse y disfrutar de un ambiente amistoso, agradable,
       \n haciendo que cada session de corte sea entretenida y relejante.
       Con nuestros servicios los clientes tendran varias opciones para elegir antes de ser atendidos y obtener el resultado esperado`,
-    servicesTitle: "Nuestros Servicios",
-    services:
-      "Brindamos servicios de calidad y asesoramiento personalizado",
-    coursesTitle: "Nuestros Cursos",
-    courses: "Ofrecemos cursos para aumentaran su experiencia",
-    contactUsTitle: "Contactenos",
-    contactUs: "Comuniquese con nosotros para mas informacion"
+    servicesTitle: 'Nuestros Servicios',
+    services: 'Brindamos servicios de calidad y asesoramiento personalizado',
+    coursesTitle: 'Nuestros Cursos',
+    courses: 'Ofrecemos cursos para aumentaran su experiencia',
+    contactUsTitle: 'Contactenos',
+    contactUs: 'Comuniquese con nosotros para mas informacion',
   };
   const services = [
     {
       icon: <FaCut className="service-icon" />,
-      name: "Cortes",
-      info:
-        "La mejor calidad de cortes modernos y clasicos",
-      url: "Read More"
+      name: 'Cortes',
+      info: 'La mejor calidad de cortes modernos y clasicos',
+      url: 'Read More',
     },
     {
       icon: <GiPaintedPottery className="service-icon" />,
-      name: "Colores",
+      name: 'Colores',
       info:
-        "Toda la gama de colores y el mejor proceso de tintado, protegiendo la salud del cabello",
-      url: "Read More"
+        'Toda la gama de colores y el mejor proceso de tintado, protegiendo la salud del cabello',
+      url: 'Read More',
     },
     {
       icon: <FaGift className="service-icon" />,
-      name: "Promos",
+      name: 'Promos',
       info:
-        "Beneficios en seleccion de servicios especiales, tales como combos y productos",
-      url: "Read More"
+        'Beneficios en seleccion de servicios especiales, tales como combos y productos',
+      url: 'Read More',
     },
   ];
   const courses = [
     {
-      name: "Curso intensivo",
-      duration: "4 meses", //  / 1 clase por semana a partir de las 13:30hrs
+      name: 'Curso intensivo',
+      duration: '4 meses', //  / 1 clase por semana a partir de las 13:30hrs
       info: `
       El curso incluye los materialez a utilizar en el mismo.
       Conoceras y entenderas las diferentes tecnicas y conceptos que \nte harán ser un barbero
@@ -117,13 +115,14 @@ const IndexPage = () => {
          - Financiado: $ 4.000 (hasta en 4 cuotas)\n\n
       Posibilidad de reservar un lugar con una seña de $1500
       `,
-      icon: "event",
-      img: "https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg",
+      icon: 'event',
+      img:
+        'https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg',
       // img: "https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg",
-      url: "Read More"
+      url: 'Read More',
     },
     {
-      name: "Perfeccionamiento de colorimentria",
+      name: 'Perfeccionamiento de colorimentria',
       info: `
       Tiene los materiales incluidos.\n
       Se realizaran los siguientes trabajos en cada uno de los modelos\n
@@ -137,45 +136,51 @@ const IndexPage = () => {
       Este curso le otorgará un certificado de concurrencia
       Fecha a coordinar 
       `,
-      duration: "6 Horas",
+      duration: '6 Horas',
       cost: 'El curso tiene un costo de $8.000',
-      icon: "event",
+      icon: 'event',
       img:
-        "https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg",
-      url: "Read More"
+        'https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg',
+      url: 'Read More',
     },
     {
-      name: "Perfeccionamiento de corte",
+      name: 'Perfeccionamiento de corte',
       info: `
         En este perfeccionamientoTrabajamos con 4 modelos,
         En los cuales haremos 3 estilos diferentes:\n 
          1 - FrenchCrop.\n 2 - Pompadour.\n 3 - Classic Cuts\n
       `,
-      duration: "2 dias", //  / 2 cortes diarios
+      duration: '2 dias', //  / 2 cortes diarios
       cost: 'El curso tiene un costo de $6.000',
-      icon: "event",
+      icon: 'event',
       img:
-        "https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg",
-      url: "Read More"
-    }
+        'https://www.inquirer.com/resizer/orhvGzXeXNLr2xpL6_d81Lv-IQE=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/PTR2I5SGUNHJJGUGMHSGXFFCKU.jpg',
+      url: 'Read More',
+    },
   ];
   const aboutusPictures = [
-    { url: 'https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/57488298_2276560875734649_7666756016645949298_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=MskQCPZA-BkAX_omsCq&oh=44ff4fcc828dcbe2403bedd48f6383e6&oe=5F57538C' },
+    {
+      url:
+        'https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/57488298_2276560875734649_7666756016645949298_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=MskQCPZA-BkAX_omsCq&oh=44ff4fcc828dcbe2403bedd48f6383e6&oe=5F57538C',
+    },
     // { url: 'https://instagram.fmvd4-1.fna.fbcdn.net/v/t51.2885-15/e35/54800426_106275450418894_8601708499349892562_n.jpg?_nc_ht=instagram.fmvd4-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=q4qz70Pjc80AX-b06U8&oh=3251c0cae9f80fbccf8e56c45667f64f&oe=5F30DAD3' }
-  ]
+  ];
   const dividers = [
     {
-      img: "https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/57488298_2276560875734649_7666756016645949298_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=MskQCPZA-BkAX_omsCq&oh=44ff4fcc828dcbe2403bedd48f6383e6&oe=5F57538C",
-      align: 'right'
+      img:
+        'https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/57488298_2276560875734649_7666756016645949298_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=MskQCPZA-BkAX_omsCq&oh=44ff4fcc828dcbe2403bedd48f6383e6&oe=5F57538C',
+      align: 'right',
     },
     {
-      img: "https://instagram.fmvd4-1.fna.fbcdn.net/v/t51.2885-15/e35/54800426_106275450418894_8601708499349892562_n.jpg?_nc_ht=instagram.fmvd4-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=q4qz70Pjc80AX-b06U8&oh=3251c0cae9f80fbccf8e56c45667f64f&oe=5F30DAD3",
-      align: 'left'
+      img:
+        'https://instagram.fmvd4-1.fna.fbcdn.net/v/t51.2885-15/e35/54800426_106275450418894_8601708499349892562_n.jpg?_nc_ht=instagram.fmvd4-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=q4qz70Pjc80AX-b06U8&oh=3251c0cae9f80fbccf8e56c45667f64f&oe=5F30DAD3',
+      align: 'left',
     },
     {
-      img: "https://scontent.fmvd4-1.fna.fbcdn.net/v/t1.0-9/s960x960/45230851_310326229560512_8082084365997178880_o.jpg?_nc_cat=110&_nc_sid=dd9801&_nc_ohc=LwkdhjYPttcAX8sw6hk&_nc_ht=scontent.fmvd4-1.fna&_nc_tp=7&oh=e90cde8ea10295ce8a00a64d7e1cae1a&oe=5F565319",
-      align: 'right'
-    }
+      img:
+        'https://scontent.fmvd4-1.fna.fbcdn.net/v/t1.0-9/s960x960/45230851_310326229560512_8082084365997178880_o.jpg?_nc_cat=110&_nc_sid=dd9801&_nc_ohc=LwkdhjYPttcAX8sw6hk&_nc_ht=scontent.fmvd4-1.fna&_nc_tp=7&oh=e90cde8ea10295ce8a00a64d7e1cae1a&oe=5F565319',
+      align: 'right',
+    },
   ];
 
   const getBarbers = async () => {
@@ -183,13 +188,15 @@ const IndexPage = () => {
     if (response) {
       barbers.map((barber: IBarber) => {
         // formatting data
-        barber.startDate = moment(barber.startDate).format('DD/MM/YYYY hh:mm:ss').substr(0, 16);
+        barber.startDate = moment(barber.startDate)
+          .format('DD/MM/YYYY hh:mm:ss')
+          .substr(0, 16);
         barber.job = 'Professional Barber';
-      })
+      });
       return response;
     }
-    return undefined
-  }
+    return undefined;
+  };
 
   return (
     <div className="index_page">
@@ -210,27 +217,28 @@ const IndexPage = () => {
           <ServicesCard
             services={services}
             title={pageInfo.servicesTitle}
-            subTitle={pageInfo.contactUs} />
+            subTitle={pageInfo.contactUs}
+          />
           <div id="barbers" />
           <BarbersCard barbers={barbers} />
           <div id="courses" />
           <CoursesCard
             courses={courses}
             title={pageInfo.coursesTitle}
-            subtitle={pageInfo.contactUs} />
+            subtitle={pageInfo.contactUs}
+          />
           <div id="contact" />
-          <Card
-            title={pageInfo.contactUsTitle}
-            subtitle={pageInfo.contactUs}>
+          <Card title={pageInfo.contactUsTitle} subtitle={pageInfo.contactUs}>
             <HomeFooter
               email={pageInfo.email}
               instagram={pageInfo.instagram}
-              facebook={pageInfo.facebook} />
+              facebook={pageInfo.facebook}
+            />
           </Card>
         </div>
-      </div >
+      </div>
       <LoaderPage show={disabled} />
-    </div >
+    </div>
   );
 };
 
