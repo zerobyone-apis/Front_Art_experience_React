00228908cee116a8a68e95f9dfca47a69a1db111
damian <damianezetiel1@gmail.com>
2020-09-02 23:53:54 -0300

    refactor in service card

--- src/components/main-page/services-card/service-card.tsx
+++ src/components/main-page/services-card/service-card.tsx
@@ -1,7 +1,28 @@
 import React, { useContext } from 'react';
 import { Card } from '../../card/card';
 import { ThemeContext } from '../../../contexts/ThemeContext';
+import { Divider } from '../../divider/divider';
 import './services-card.scss';
+import '../../../styles/theme.scss';
+
+export const ServiceItem = (props: {
+    key?: number,
+    icon: any,
+    name: string,
+    info: string
+}) => {
+    const {
+        // @ts-ignore
+        getTheme,
+    } = useContext(ThemeContext);
+    return (
+        <div className={`service-item`} key={props.key}>
+            {props.icon}
+            <p className={`service-name text text-light`}>{props.name}</p>
+            <p className={`service-info text text-light`}>{props.info}</p>
+        </div>
+    )
+}
 
 export const ServicesCard = (props: {
     services: any[],
@@ -15,18 +36,23 @@ export const ServicesCard = (props: {
 
     const getServices = () => {
         return props.services.map((service, i) =>
-            <div className={`service-item`} key={i}>
-                {service.icon}
-                <p className={`service-name text text-${getTheme()}`}>{service.name}</p>
-                <p className={`service-info text text-${getTheme()}`}>{service.info}</p>
-            </div>
+            <ServiceItem key={i}
+                name={service.name}
+                info={service.info}
+                icon={service.icon}
+            />
         )
     }
 
     return (
-        <Card className="services-card" title={props.title} subtitle={props.subTitle}>
-            {getServices()}
-        </Card>
+        <Divider img="https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/57488298_2276560875734649_7666756016645949298_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=MskQCPZA-BkAX_omsCq&oh=44ff4fcc828dcbe2403bedd48f6383e6&oe=5F57538C"
+            align="left">
+            <div className="divider-content">
+                <h1 className="title">Brindamos servicios de alto nivel, teniendo en cuenta sus necesidades</h1>
+                <div className="services-card">
+                    {getServices()}
+                </div>
+            </div>
+        </Divider>
     )
-
 }
