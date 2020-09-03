f1eebdc3a405710879a55e75ccd33fdb36b8c6e8
damian <damianezetiel1@gmail.com>
2020-09-02 23:55:00 -0300

    refactor in courses-card

--- src/components/main-page/courses-card/course-card.tsx
+++ src/components/main-page/courses-card/course-card.tsx
@@ -3,6 +3,7 @@ import { Card } from '../../card/card';
 import { ThemeContext } from '../../../contexts/ThemeContext';
 import { Button } from '../../button/button';
 import { DialogModal } from '../../dialog-modal/dialog-modal';
+import { Divider } from '../../divider/divider';
 import './courses-card.scss';
 import '../../../styles/theme-buttons.scss';
 
@@ -35,25 +36,37 @@ export const CoursesCard = (props: {
     }
 
     return (
-        <Fragment>
-            <Card className="courses-card" title={props.title} subtitle={props.subtitle}>
-                {getCourses()}
-            </Card>
-            {
-                !showDialog ? null :
-                    <DialogModal
-                        className="dialog"
-                        title={selected.name}
-                        onClose={() => { setShowDialog(false) }} >
-                        {selected.info.split('\n').map((item, i) => {
-                            return <p className={`course-info text text-${getTheme()}`} key={i}>{item}</p>
-                        })}
-                        <div className="divider"></div>
-                        {selected.cost.split('\n').map((item, i) => {
-                            return <p className={`course-info text text-${getTheme()}`} key={i}>{item}</p>
-                        })}
-                    </DialogModal>
-            }
-        </Fragment>
+        <Divider img="https://scontent.fmvd4-1.fna.fbcdn.net/v/t1.0-9/s960x960/45230851_310326229560512_8082084365997178880_o.jpg?_nc_cat=110&_nc_sid=dd9801&_nc_ohc=LwkdhjYPttcAX8sw6hk&_nc_ht=scontent.fmvd4-1.fna&_nc_tp=7&oh=e90cde8ea10295ce8a00a64d7e1cae1a&oe=5F565319"
+            align="left">
+            <div className="divider-content">
+                <h1 className="title">Nuestros cursos estan enfocados a personas que desean aprender con la mejor atencion y rapidez</h1>
+                <div className="courses-card">
+                    {getCourses()}
+                </div>
+            </div>
+        </Divider>
+
+
+
+        // <Fragment>
+        //     <Card className="courses-card" title={props.title} subtitle={props.subtitle}>
+        //         {getCourses()}
+        //     </Card>
+        //     {
+        //         !showDialog ? null :
+        //             <DialogModal
+        //                 className="dialog"
+        //                 title={selected.name}
+        //                 onClose={() => { setShowDialog(false) }} >
+        //                 {selected.info.split('\n').map((item, i) => {
+        //                     return <p className={`course-info text text-${getTheme()}`} key={i}>{item}</p>
+        //                 })}
+        //                 <div className="divider"></div>
+        //                 {selected.cost.split('\n').map((item, i) => {
+        //                     return <p className={`course-info text text-${getTheme()}`} key={i}>{item}</p>
+        //                 })}
+        //             </DialogModal>
+        //     }
+        // </Fragment>
     );
 }
