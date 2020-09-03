7ad9e743ed22d952dd2643ee72801004aa5b8d69
damian <damianezetiel1@gmail.com>
2020-09-03 20:08:37 -0300

    refactor couse card

--- src/components/main-page/courses-card/course-card.tsx
+++ src/components/main-page/courses-card/course-card.tsx
@@ -6,6 +6,7 @@ import { DialogModal } from '../../dialog-modal/dialog-modal';
 import { Divider } from '../../divider/divider';
 import './courses-card.scss';
 import '../../../styles/theme-buttons.scss';
+import { ContainerPage } from '../container-page/container-page';
 
 export const CoursesCard = (props: {
     courses: any[],
@@ -21,17 +22,12 @@ export const CoursesCard = (props: {
 
     const getCourses = () => {
         return props.courses.map((course, i) =>
-            <div className="course-item" key={i}>
-                <p className={`course-name text text-${getTheme()}`}>{course.name}</p>
-                <img className="course-img" aspect-ratio="1" src={course.img}></img>
-                <p className={`course-duration text text-${getTheme()}`}>Duracion: {course.duration}</p>
-                <Button className="see_more-btn theme-button-outlined"
-                    label="Ver mas"
-                    onClick={() => {
-                        setSelected(course);
-                        setShowDialog(true);
-                    }} />
-            </div >
+            <ContainerPage className="container-page"
+                key={i}
+                title={course.name}
+                info={course.info}
+                img={course.img}
+                align={"left"} />
         )
     }
 
@@ -39,7 +35,8 @@ export const CoursesCard = (props: {
         <Divider img="https://scontent.fmvd4-1.fna.fbcdn.net/v/t1.0-9/s960x960/45230851_310326229560512_8082084365997178880_o.jpg?_nc_cat=110&_nc_sid=dd9801&_nc_ohc=LwkdhjYPttcAX8sw6hk&_nc_ht=scontent.fmvd4-1.fna&_nc_tp=7&oh=e90cde8ea10295ce8a00a64d7e1cae1a&oe=5F565319"
             align="left">
             <div className="divider-content">
-                <h1 className="title">Nuestros cursos estan enfocados a personas que desean aprender con la mejor atencion y rapidez</h1>
+                {/* Nuestros cursos estan enfocados a personas que desean aprender con la mejor atencion y rapidez */}
+                <h1 className="divider-title">Nuestros Cursos</h1>
                 <div className="courses-card">
                     {getCourses()}
                 </div>
