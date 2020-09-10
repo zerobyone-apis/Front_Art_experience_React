9e0dcf9a53bd9f1bf183420a71d72e10c1bafb23
damian <damianezetiel1@gmail.com>
2020-09-10 19:25:04 -0300

    refactor course-card

--- src/components/main-page/courses-card/course-card.tsx
+++ src/components/main-page/courses-card/course-card.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, Fragment } from 'react';
 import { ThemeContext } from '../../../contexts/ThemeContext';
 import { Divider } from '../../divider/divider';
-import { ContainerPage } from '../container-page/container-page';
+import { ContainerPage, SubContainerInfo, SubContainerImage } from '../../test/container-page/container-page';
 import { useWindowSize } from '../../../hooks/useWindowSize';
 import './courses-card.scss';
 import '../../../styles/theme-buttons.scss';
@@ -13,7 +13,7 @@ export const CoursesCard = (props: {
     subTitle: string
 }) => {
 
-    const [selectedCourse, setSelectedCourse] = useState(undefined);
+    const [selectedCourse, setSelectedCourse] = useState(props.courses[0]);
     const [effects, setEffects] = useState('');
     const screenSize = useWindowSize();
 
@@ -63,38 +63,30 @@ export const CoursesCard = (props: {
 
     return (
         <Divider
-            title="Nuestros Cursos"
+            title="Nuestros Barberos"
             img="https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/40756317_547596808994027_4028564252884205568_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=G9NtG88ekgYAX-ZqNF_&oh=6b1c52b718d5107bcbd7f30a4233ac08&oe=5F7EA78C"
             align="left"
             className="divider"
         >
             <div className="course-card">
-                <div className="courses-items">
-                    <p className="help-action">Cursos</p>
-                    {getCourses()}
-                </div>
-                <div className="course-info">
-                    {selectedCourse ?
-                        (
-                            <ContainerPage
-                                className={`couse-container ${effects}`}
-                                align="left"
-                                title={selectedCourse.name}
-                                info={selectedCourse.info}
-                                img={selectedCourse.img} />
-                        ) : (
-                            <ContainerPage
-                                className="couse-container"
-                                align="left"
-                                title={``}
-                                info={`Nuestros cursos son la mejor opcion para entrar en el mundo de la barberia profesional.\n
-                                Contamos con 3 cursos con la mejor calidad y asesoramiento personalizado.\n
-                                -Curso Intensivo\n
-                                -Perfeccionamiento de Colorimetria\n
-                                -Perfeccionamiento de Corte`}
-                                img={"https://instagram.fmvd1-1.fna.fbcdn.net/v/t51.2885-15/e35/43984597_349284282317504_392973023499517952_n.jpg?_nc_ht=instagram.fmvd1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=PKoXQpRr41AAX8wq00j&oh=db7ea1933aaf5c5bd91254931763c15f&oe=5F7BC29B"} />
-                        )}
-                </div>
+                <ContainerPage
+                    className={`container-course`}
+                    containerClassName={`${effects}`}
+                    leftContent={
+                        <div className="courses-items">
+                            <p className="help-action">Barberos</p>
+                            {getCourses()}
+                        </div>
+                    }
+                >
+                    <SubContainerInfo
+                        title={selectedCourse.name}
+                        info={selectedCourse.info}
+                    />
+                    <SubContainerImage
+                        img={selectedCourse.img}
+                    />
+                </ContainerPage>
             </div>
         </Divider>
     )
