25c4f9f1330764eccad35750f2fbcb8bf0f241e4
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-09-20 22:14:31 -0300

    Update Reserves Actions Update + Cancel + Done

--- src/actions/Reserve.actions.ts
+++ src/actions/Reserve.actions.ts
@@ -1,18 +1,20 @@
 import IntegrationBackend from '../utils/IntegrationBackend';
 import ResultObject from '../utils/ResultObject';
-import { RESERVE_ROUTE, POST_ENDPOIT, GET_ENDPOIT } from '../types/Routes.type';
+import {
+  RESERVE_ROUTE,
+  POST_ENDPOIT,
+  GET_ENDPOIT,
+  PUT_ENDPOIT,
+  PATCH_ENDPOIT,
+} from '../types/Routes.type';
 // eslint-disable-next-line no-unused-vars
 import { IReserve } from '../types/Reserve.type';
-import moment from 'moment';
 
 export default class ReserveActions {
   private backend: IntegrationBackend = new IntegrationBackend();
 
   public async add(reserve: IReserve) {
     console.log('Accede Reserve Actions', reserve);
-
-    const customMoment = moment;
-    customMoment.locale();
     try {
       const response: any = await this.backend.send(
         POST_ENDPOIT,
@@ -43,6 +45,88 @@ export default class ReserveActions {
     } catch (error) {
       console.error('Error Reserve.actions method add -> ', error.message);
       return null;
+    }
+  }
+
+  public async update(reserve: IReserve) {
+    /*
+    Example of Reserve OBJ TO UPDATE:
+    {   
+        "reserveId":1,
+        "barberOrHairdresserId": 1,
+        "clientId": 1,
+        "celClient": 94159988,
+        "mailClient": "put.test.franco.salvohotmail.com",
+        "workToDo": "CORTE+BARBA+CEJAS",
+        "priceWork": 350,
+        "additionalCost": 40,
+        "startTime": "2020-09-21T12:00:14.396Z"
+    }
+    */
+    try {
+      const response: any = await this.backend.send(
+        PUT_ENDPOIT,
+        reserve,
+        `${RESERVE_ROUTE}/update`
+      );
+      if (response.status !== 201) {
+        console.log(response.message);
+        throw Error(response.message);
+      }
+      console.log('Update reserve successfully: \n', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error Reserve.actions method update -> ', error.message);
+      return new ResultObject(404, error.message, {});
+    }
+  }
+
+  public async doneReserve(barberId: number, reserveId: number) {
+    /**
+     * URL TO DONE RESERVE: /barber/{id_barber}/isdone/{id_reserve}
+     */
+    try {
+      const response: any = await this.backend.send(
+        PATCH_ENDPOIT,
+        null,
+        `${RESERVE_ROUTE}/barber/${barberId}/isDone/${reserveId}`
+      );
+      if (response.status !== 201) {
+        console.log(response.message);
+        throw Error(response.message);
+      }
+      console.log('Completed reserve successfully: \n', response.data);
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Error Reserve.actions method doneReserve -> ',
+        error.message
+      );
+      return new ResultObject(404, error.message, {});
+    }
+  }
+
+  public async cancel(clientId: number, reserveId: number) {
+    /*
+       
+        URL TO CANCEL:  /reserve/client/${clientId}/cancel/${reserveId}
+    
+    */
+    try {
+      const response: any = await this.backend.send(
+        PATCH_ENDPOIT,
+        null,
+        `${RESERVE_ROUTE}/client/${clientId}/cancel/${reserveId}`
+      );
+      if (response.status !== 201) {
+        console.log(response.message);
+        throw Error(response.message);
+      }
+      console.log('Canceled reserve successfully: \n', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error Reserve.actions method cancel -> ', error.message);
+      return new ResultObject(404, error.message, {});
     }
   }
 }
--- src/components/dashboard/reserve-dialog/reserve-dialog.tsx
+++ src/components/dashboard/reserve-dialog/reserve-dialog.tsx
@@ -1,122 +1,168 @@
-import React, { useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { DialogModal } from '../../dialog-modal/dialog-modal';
 import ReserveActions from '../../../actions/Reserve.actions';
 import { IReserve } from '../../../types/Reserve.type';
 import { ValidationForm } from '../../validation-form/validation-form';
 import { Button } from '../../button/button';
 import { TextField } from '../../text-field/text-field';
+import { ButtonContext } from '../../../contexts/ButtonsContext';
 import moment from 'moment';
 import './reserve-dialog.scss';
 import '../../../styles/theme-buttons.scss';
 import '../../../styles/effects.scss';
 
-export const ReserveDialog = (props: {
-    reserve: IReserve,
-    onClose: any
-}) => {
-    const baseReserve: IReserve = {
-        barberOrHairdresserId: -1,
-        celClient: '',
-        clientId: -1,
-        mailClient: '',
-        nameClient: '',
-        priceWork: 0,
-        startTime: '',
-        additionalCost: 0,
+export const ReserveDialog = (props: { reserve: IReserve; onClose: any }) => {
+  const baseReserve: IReserve = {
+    barberOrHairdresserId: -1,
+    celClient: '',
+    clientId: -1,
+    mailClient: '',
+    nameClient: '',
+    priceWork: 0,
+    startTime: '',
+    additionalCost: 0,
+  };
+  const {
+    // @ts-ignore
+    disabled,
+    setDisabledButton,
+  } = useContext(ButtonContext);
+
+  const reserveActions: ReserveActions = new ReserveActions();
+  const [showDialog, setShowDialog] = useState(false);
+  const [reserve, setReserve] = useState(props.reserve || baseReserve);
+  const [updated, setUpdated] = useState(false);
+
+  const onChangeReserve = (value: string, fieldName: string) => {
+    setReserve({ ...reserve, [fieldName]: value });
+  };
+
+  const onUpdate = async () => {
+    setDisabledButton(true);
+    let formatDate = moment(reserve.startTime).format('YYYY-MM-DDTHH:mm:ss');
+    console.log('Format date -> to Update', formatDate);
+    let reserveUpdate: IReserve = {
+      reserveId: reserve.reserveId,
+      nameClient: reserve.nameClient,
+      barberOrHairdresserId: reserve.barberOrHairdresserId,
+      clientId: reserve.clientId,
+      celClient: reserve.celClient,
+      mailClient: reserve.mailClient,
+      workToDo: reserve.workToDo,
+      priceWork: reserve.priceWork,
+      additionalCost: reserve.additionalCost,
+      startTime: formatDate,
+    };
+
+    const response = await reserveActions.update(reserveUpdate);
+
+    if (response) {
+      setUpdated(true);
+      setDisabledButton(false);
     }
-    const [showDialog, setShowDialog] = useState(false);
-    const [reserve, setReserve] = useState(props.reserve || baseReserve)
-    const reserveActions: ReserveActions = new ReserveActions();
+  };
 
-    const onChangeReserve = (value: string, fieldName: string) => {
-        setReserve({ ...reserve, [fieldName]: value })
-    }
+  const isDone = () => {};
+  const onCancel = () => {};
 
-    return (
-        <DialogModal
-            className="reserve-dialog"
-            title="Control de Reserva"
-            showModal={showDialog}
-            onClose={props.onClose}>
-            <div className="reserve-modal">
-                <div className="reserve_data-box">
-                    <p className="reserve_info effect-slide_left">Datos del Cliente</p>
-                    <ValidationForm
-                        objectTest={reserve}
-                        buttonLabel="Guardar Cambios"
-                        buttonClassName="access_btn theme-button-outlined"
-                        onClick={() => {
-                            // login()
-                            alert('click')
-                        }}
-                    >
-                        <TextField
-                            value={reserve.nameClient}
-                            name="nameClient"
-                            type="string"
-                            required={true}
-                            label="Nombre"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                        <TextField
-                            value={reserve.mailClient}
-                            name="mailClient"
-                            type="email"
-                            required={true}
-                            label="Email"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                        <TextField
-                            value={reserve.celClient}
-                            name="celClient"
-                            type="number"
-                            required={true}
-                            label="Celular"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                        <p className="reserve_info">Datos de la Reserva</p>
-                        <TextField
-                            value={reserve.startTimeFront}
-                            name="startTimeFront"
-                            type="allow"
-                            required={true}
-                            label="Fecha y hora"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                        <TextField
-                            value={reserve.barberOrHairdresserId}
-                            name="barberOrHairdresserId"
-                            type="allow"
-                            required={true}
-                            label="Barbero"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                        <TextField
-                            value={reserve.workToDo}
-                            name="workToDo"
-                            type="allow"
-                            required={true}
-                            label="Servicio"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                        <TextField
-                            value={reserve.totalCost}
-                            name="totalCost"
-                            type="number"
-                            required={true}
-                            label="Costo Total"
-                            className="theme-text_field--dark"
-                            onChange={onChangeReserve} />
-                    </ValidationForm>
-                </div>
-            </div>
+  return (
+    <DialogModal
+      className="reserve-dialog"
+      title="Control de Reserva"
+      showModal={showDialog}
+      onClose={props.onClose}
+    >
+      <div className="reserve-modal">
+        <div className="reserve_data-box">
+          <p className="reserve_info effect-slide_left">Datos del Cliente</p>
+          <ValidationForm
+            objectTest={reserve}
+            buttonLabel="Guardar Cambios"
+            buttonClassName="access_btn theme-button-outlined"
+            onClick={onUpdate}
+          >
+            <TextField
+              value={reserve.nameClient}
+              name="nameClient"
+              type="string"
+              required={true}
+              label="Nombre"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+            <TextField
+              value={reserve.mailClient}
+              name="mailClient"
+              type="email"
+              required={true}
+              label="Email"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+            <TextField
+              value={reserve.celClient}
+              name="celClient"
+              type="number"
+              required={true}
+              label="Celular"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+            <p className="reserve_info">Datos de la Reserva</p>
+            <TextField
+              value={reserve.startTimeFront}
+              name="startTimeFront"
+              type="allow"
+              required={true}
+              label="Fecha y hora"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+            <TextField
+              value={reserve.barberOrHairdresserId}
+              name="barberOrHairdresserId"
+              type="allow"
+              required={true}
+              label="Barbero"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+            <TextField
+              value={reserve.workToDo}
+              name="workToDo"
+              type="allow"
+              required={true}
+              label="Servicio"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+            <TextField
+              value={reserve.totalCost}
+              name="totalCost"
+              type="number"
+              required={true}
+              label="Costo Total"
+              className="theme-text_field--dark"
+              onChange={onChangeReserve}
+            />
+          </ValidationForm>
+        </div>
+      </div>
 
-            <div className="footer">
-                <div className="footer_right-box">
-                    <Button className="footer-button theme-button-outlined" label="Finalizar" onClick={() => { }} />
-                    <Button className="footer-button theme-button-outlined" label="Cancelar" onClick={() => { }} />
-                </div>
-            </div>
-        </DialogModal >
-    );
-}
+      <div className="footer">
+        <div className="footer_right-box">
+          <Button
+            className="footer-button theme-button-outlined"
+            label="Finalizar"
+            onClick={() => {}}
+          />
+          <Button
+            className="footer-button theme-button-outlined"
+            label="Cancelar"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+    </DialogModal>
+  );
+};
