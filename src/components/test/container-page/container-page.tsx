8b51999b24ec99f50a0913aee9a57af1b8898d44
damian <damianezetiel1@gmail.com>
2020-09-28 03:39:44 -0300

    fixes in container page in test folder

--- src/components/test/container-page/container-page.tsx
+++ src/components/test/container-page/container-page.tsx
@@ -1,4 +1,4 @@
-import React, { ReactChild, Fragment } from 'react';
+import React, { ReactChild, Fragment, ReactElement } from 'react';
 import { useWindowSize } from '../../../hooks/useWindowSize';
 import './container-page.scss';
 import { Grid } from '@material-ui/core';
@@ -32,21 +32,23 @@ export const SubContainerInfo = (props: {
           })}
         </p>
       ) : (
-        ''
-      )}
+          ''
+        )}
     </div>
   );
 };
 
 export const SubContainerImage = (props: {
-  imgClassName?: string;
-  img: string;
-  imgFooter?: any;
+  imgClassName?: string,
+  img: string,
+  imgFooter?: any,
+  title?: string,
 }) => {
   const screenSize = useWindowSize();
   return (
     <div className={`img-box ${props.imgClassName}`}>
       <div className="image">
+        <p className={`title`}>{props.title}</p>
         {typeof props.img === 'string' ? (
           <Fragment>
             <img
@@ -62,29 +64,40 @@ export const SubContainerImage = (props: {
             ) : null}
           </Fragment>
         ) : (
-          props.img
-        )}
+            props.img
+          )}
       </div>
     </div>
   );
 };
 
+
 export const ContainerPage = (props: {
-  children: ReactChild[];
+  children: ReactChild | ReactChild[];
   leftContent?: any;
   RightContent?: Element;
   key?: number;
   className?: string;
   containerClassName?: string;
 }) => {
+
+  const getTotalChilds = () => {
+    let childs: any = props.children;
+    try {
+      return childs.length;
+    } catch (error) {
+      return 1
+    }
+  }
+
   return (
     <div key={props.key} className={`container-page ${props.className}`}>
       <div className="left-box">{props.leftContent}</div>
       <div className={`containers ${props.containerClassName}`}>
-        {props.children.map((child, i) => {
+        {React.Children.map(props.children, (child: ReactElement, i: number) => {
           return (
             <div
-              style={{ width: `${100 / props.children.length}%` }}
+              style={{ width: `${100 / getTotalChilds()}%`, margin: `${getTotalChilds() != 1 ? 'auto' : 'none'}` }}
               key={i}
               className="sub-container"
             >
