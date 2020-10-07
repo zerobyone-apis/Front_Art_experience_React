7f5d750c913e8d576dc2ac2f8a01a887b823b241
damian <damianezetiel1@gmail.com>
2020-10-07 17:12:38 -0300

    fix style mobile

--- src/components/reserve-modal/reserve-modal.tsx
+++ src/components/reserve-modal/reserve-modal.tsx
@@ -123,9 +123,8 @@ export const ReserveModal = (props: { className?: string }) => {
   const reserveActions = new ReserveActions();
 
   const createReserve = async () => {
-    const startDateFormatted = `${
-      moment(reserveDate).format().split('T')[0]
-    }T${reserveHour}:00`;
+    const startDateFormatted = `${moment(reserveDate).format().split('T')[0]
+      }T${reserveHour}:00`;
     const newReserve: IReserve = {
       barberOrHairdresserId: selectedBarber.barberId,
       clientId: getUserData().clientId,
@@ -407,11 +406,11 @@ export const ReserveModal = (props: { className?: string }) => {
               onNextButtonClick={
                 wizard < totalWizard
                   ? () => {
-                      setWizard(wizard + 1);
-                    }
+                    setWizard(wizard + 1);
+                  }
                   : () => {
-                      createReserve();
-                    }
+                    createReserve();
+                  }
               }
               onPrevButtonClick={() => {
                 setWizard(wizard - 1);
