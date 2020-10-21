44c359250f78dcb42150f3bc3a3b57e0e50938ca
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-10-21 16:59:09 -0300

    Adding next pluggins to fix deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-const withStyles = require('@webdeb/next-styles');
-module.exports = withStyles({
-  sass: true,
-  modules: true,
-});
+//const withStyles = require('@webdeb/next-styles');
+//const withStylus = require("@zeit/next-stylus");
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+
+//module.exports = withStylus({
+//  sass: true,
+//  modules: true,
+//});
+
+module.exports = withCSS(
+  withSass({
+    sass: true,
+    modules: true,
+  })
+);
