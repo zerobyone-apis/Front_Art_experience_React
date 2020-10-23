ae7817453128277a94dc026c0d5343c0cd9bc721
Maximiliano Nicolas Olivero <maximiliano.olivero@globant.com>
2020-10-23 13:51:23 -0300

    fix deploy adding @zeit/node-sass

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { CustomTable } from '../components/dashboard/custom-table/custom-table';
-import { ButtonContext } from '../contexts/ButtonsContext';
-import { ReserveDialog } from '../components/dashboard/reserve-dialog/reserve-dialog';
-import { Toolbar } from '../components/dashboard/toolbar/toolbar';
-import { LoaderPage } from '../components/loader-page/loader-page';
-import { IReserve } from '../types/Reserve.type';
-import ReserveActions from '../actions/Reserve.actions';
-import { AiOutlineSend } from 'react-icons/ai';
-import { UserContext } from '../contexts/UserContext';
-import moment from 'moment';
-import './Dashboard.scss';
-import '../styles/theme.scss';
+import React, { useState, useEffect, useContext } from "react";
+import { CustomTable } from "../components/dashboard/custom-table/custom-table";
+import { ButtonContext } from "../contexts/ButtonsContext";
+import { ReserveDialog } from "../components/dashboard/reserve-dialog/reserve-dialog";
+import { Toolbar } from "../components/dashboard/toolbar/toolbar";
+import { LoaderPage } from "../components/loader-page/loader-page";
+import { IReserve } from "../types/Reserve.type";
+import ReserveActions from "../actions/Reserve.actions";
+import { AiOutlineSend } from "react-icons/ai";
+import { UserContext } from "../contexts/UserContext";
+import moment from "moment";
+import "./Dashboard.scss";
+import "../styles/theme.scss";
 
 export const listItems = (props: { items: string[] }) => {
   return (
@@ -42,41 +42,37 @@ const DashboardPage = () => {
   } = useContext(ButtonContext);
   const {
     // @ts-ignore
-    userIsAdmin
+    userIsAdmin,
   } = useContext(UserContext);
-
-
 
   /* validation of access into Dashboard page */
   /* Is necesary be a admin */
   useEffect(() => {
-    !userIsAdmin() ? document.location.href = '/' : null;
+    !userIsAdmin() ? (document.location.href = "/") : null;
   }, []);
 
-
-
   const headerOrder = [
-    { text: 'ID', value: 'reserveId' },
-    { text: 'N°Social', value: 'socialNumber' },
-    { text: 'Cliente', value: 'nameClient' },
-    { text: 'Fecha', value: 'startTimeFront' },
-    { text: 'Barbero', value: 'barberName' },
-    { text: 'Servicio', value: 'workToDo' },
+    { text: "ID", value: "reserveId" },
+    { text: "N°Social", value: "socialNumber" },
+    { text: "Cliente", value: "nameClient" },
+    { text: "Fecha", value: "startTimeFront" },
+    { text: "Barbero", value: "barberName" },
+    { text: "Servicio", value: "workToDo" },
   ];
 
   const services = [
-    'Cortes para Caballeros',
-    'Cortes para Damas',
-    'Servicio de Cafetería',
-    'Productos',
+    "Cortes para Caballeros",
+    "Cortes para Damas",
+    "Servicio de Cafetería",
+    "Productos",
   ];
-  const courses = ['Intensivo', 'Colorimetría', 'Avanzado'];
+  const courses = ["Intensivo", "Colorimetría", "Avanzado"];
 
   const mobileHeaders = [headerOrder[1], headerOrder[2], headerOrder[3]];
   useEffect(() => {
     const fetchData = async () => {
       await getReserves();
-    }
+    };
     fetchData();
   }, []);
 
@@ -88,7 +84,7 @@ const DashboardPage = () => {
       // formatting date
       reserves.map((reserve: IReserve) => {
         reserve.startTimeFront = moment(reserve.startTime)
-          .format('DD/MM/YYYY hh:mm:ss')
+          .format("DD/MM/YYYY hh:mm:ss")
           .substr(0, 16);
       });
       setReserve(reserves);
@@ -114,8 +110,8 @@ const DashboardPage = () => {
               headers={headerOrder}
               mobileHeaders={mobileHeaders}
               sortColumnByOtherHeader={{
-                headerToAction: 'startTimeFront',
-                headerToSort: 'reserveId',
+                headerToAction: "startTimeFront",
+                headerToSort: "reserveId",
               }}
               onSelectRow={showSelectedReserve}
             />
@@ -126,12 +122,12 @@ const DashboardPage = () => {
       {selectedReserve && showReserveDialog ? (
         <ReserveDialog
           reserve={selectedReserve}
-          onUpdated={(async (updated) => {
+          onUpdated={async (updated) => {
             /* Forma no recomendada pero para salir del paso */
             setReserve([]);
             await getReserves();
             setShowReserveDialog(false);
-          })}
+          }}
           onClose={setShowReserveDialog}
         />
       ) : null}
@@ -139,5 +135,5 @@ const DashboardPage = () => {
   );
 };
 
-DashboardPage.displayName = 'Dashboard Page';
+DashboardPage.displayName = "Dashboard Page";
 export default DashboardPage;
