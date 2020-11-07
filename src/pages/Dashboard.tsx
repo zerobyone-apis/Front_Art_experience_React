7391c6968c75ad17f3ee4666b4cb9c8144191153
damian <damianezetiel1@gmail.com>
2020-11-07 19:08:45 -0300

    Dashboard - Refactor structure

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,37 @@
 import React, { useState, useEffect, useContext } from "react";
-import { CustomTable } from "../components/dashboard/custom-table/custom-table";
 import { ButtonContext } from "../contexts/ButtonsContext";
-import { ReserveDialog } from "../components/dashboard/reserve-dialog/reserve-dialog";
 import { Toolbar } from "../components/dashboard/toolbar/toolbar";
 import { LoaderPage } from "../components/loader-page/loader-page";
-import { IReserve } from "../types/Reserve.type";
 import { UserContext } from "../contexts/UserContext";
-import { headerOrder } from '../data/dashboard';
-import ReserveActions from "../actions/Reserve.actions";
-import moment from "moment";
+import { BiCalendarEvent } from 'react-icons/bi';
+import { DiGoogleAnalytics } from 'react-icons/di';
+import { IVerticalToolbarItem, VerticalToolbar } from "../components/dashboard/vertical-toolbar";
+import { ReserveManager } from "../components/dashboard/reserves-manager";
 import "./Dashboard.scss";
-import "../styles/theme.scss";
 
 const DashboardPage = () => {
-  const [reserves, setReserve] = useState([]);
-  const [selectedReserve, setSelectedReserve] = useState(undefined);
-  const [showReserveDialog, setShowReserveDialog] = useState(false);
+  // Wizard is used for set the actual tool
+  const [wizard, setWizard] = useState(0);
 
-  const reserveActions: ReserveActions = new ReserveActions();
-  const mobileHeaders = [headerOrder[1], headerOrder[2], headerOrder[3]];
+
+  const verticalToolbarItems: IVerticalToolbarItem[] = [
+    {
+      icon: <BiCalendarEvent />,
+      tool: <ReserveManager />,
+      label: 'Administracion de Reservas',
+    },
+    {
+      icon: <DiGoogleAnalytics />,
+      tool: <div />,
+      label: 'Estadisticas'
+    }
+  ];
+
+
+  const getToolByWizard = (wizard: number) => {
+    return verticalToolbarItems[wizard].tool;
+  }
+
 
   const {
     // @ts-ignore
@@ -32,78 +45,27 @@ const DashboardPage = () => {
 
 
   useEffect(() => {
-    const fetchData = async () => {
-      await getReserves();
-    };
     // check user is admin
-    !userIsAdmin() ? (document.location.href = "/") : (
-      // false ? (document.location.href = "/") : (
-      // get reserves
-      fetchData()
-    );
+    // !userIsAdmin() ? (document.location.href = "/") : (
+    false && (document.location.href = "/")
   }, []);
 
 
-  const getReserves = async () => {
-    setDisabledButton(true);
-    const reserves: any[] = await reserveActions.getAll();
-    setDisabledButton(false);
-    if (reserves) {
-      // formatting date
-      reserves.map((reserve: IReserve) => {
-        reserve.startTimeFront = moment(reserve.startTime)
-          .format("DD/MM/YYYY hh:mm:ss")
-          .substr(0, 16);
-      });
-      setReserve(reserves);
-    }
-  };
-
-
-  const showSelectedReserve = (reserve: any) => {
-    setSelectedReserve(reserve);
-    setShowReserveDialog(true);
-  };
-
-
   return (
-    <div className="dashboard_page">
+    <div className="dashboard-page">
       <Toolbar />
-      <div className="page-box">
-        <div className="dashboard">
-          {reserves.length ? (
-            <CustomTable
-              title="Lista de Reservas"
-              noItemsMessage="No tiene reservas creadas"
-              noSearchMessage="No se encontraron coincidencias"
-              items={reserves}
-              headers={headerOrder}
-              mobileHeaders={mobileHeaders}
-              sortColumnByOtherHeader={{
-                headerToAction: "startTimeFront",
-                headerToSort: "reserveId",
-              }}
-              onSelectRow={showSelectedReserve}
-            />
-          ) : null}
-        </div>
+      <div className="container">
+        <VerticalToolbar
+          items={verticalToolbarItems}
+          onSelectItem={setWizard}
+        />
+        {getToolByWizard(wizard)}
       </div>
       <LoaderPage show={disabled} />
-      {selectedReserve && showReserveDialog ? (
-        <ReserveDialog
-          reserve={selectedReserve}
-          onUpdated={async (updated) => {
-            /* Forma no recomendada pero para salir del paso */
-            setReserve([]);
-            await getReserves();
-            setShowReserveDialog(false);
-          }}
-          onClose={setShowReserveDialog}
-        />
-      ) : null}
     </div>
   );
 };
 
-DashboardPage.displayName = "Dashboard Page";
+
+// DashboardPage.displayName = "Dashboard Page";
 export default DashboardPage;
