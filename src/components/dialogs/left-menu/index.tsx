ec3630009b15dcf3f0f427aa52760023b07dcb75
damian <damianezetiel1@gmail.com>
2020-11-21 05:30:50 -0300

    fix left menu style

--- src/components/dialogs/left-menu/index.tsx
+++ src/components/dialogs/left-menu/index.tsx
@@ -1,56 +1,58 @@
 import React, { useState } from 'react';
-import { Button } from '../button';
+import { Button } from '../../inputs/button';
 import { FiMenu } from 'react-icons/fi';
 import { DialogModal } from '../dialog-modal/dialog-modal';
-import { toolbarButtons } from '../../utils/toolbarButtons';
+import { toolbarButtons } from '../../../utils/toolbarButtons';
 import './left-menu.scss';
-import '../../theme/effects.scss';
+
 
 export const LeftMenu = () => {
+
   const [showDialog, setShowDialog] = useState(false);
+
   return (
-    <div className="left_menu">
+    <div className="left-menu">
+
+      {/* BUTTON ACTIVATOR */}
       <Button
         style="text"
-        className="activator_btn"
+        className="left-menu-activator-btn"
         icon={<FiMenu className="theme-icon icon_button" />}
         onClick={() => {
           setShowDialog(true);
         }}
       />
-      {!showDialog ? null : (
+
+      {showDialog && (
         <DialogModal
-          onClose={() => {
-            setShowDialog(false);
-          }}
-          className="left_menu-dialog"
-        >
-          <div className="left-menu-box effect-slide-left">
-            <div className="logo">
-              <img
-                className="logo-img"
-                src="https://i.ibb.co/8g4h8sk/A-art-experiecnce.png"
-                alt=""
-              />
-            </div>
+          onClose={() => setShowDialog(false)}
+          className="left-menu-dialog"
+          fullscreen={true}
+          fullscreenOnMobile={true}>
+
+          <div className="logo">
+            <img src="https://i.ibb.co/8g4h8sk/A-art-experiecnce.png" alt="" />
+          </div>
+
+          <div className="left-menu-box">
             {toolbarButtons.map((button, i) => {
               return (
                 <Button
                   style="text"
+                  textStyle="title"
                   key={i}
                   href={button.href}
-                  onClick={() => {
-                    setShowDialog(false);
-                  }}
-                  className="left_menu-btn"
+                  onClick={() => setShowDialog(false)}
+                  className="left-menu-btn"
                   label={button.label}
                 />
-              );
+              )
             })}
           </div>
+
           <div />
         </DialogModal>
       )}
     </div>
-  );
-};
+  )
+}
