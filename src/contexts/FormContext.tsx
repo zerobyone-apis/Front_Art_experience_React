38a37d738ad0ebb11ae7600de534899b99182af2
damian <damianezetiel1@gmail.com>
2020-11-21 05:25:52 -0300

    refactor in FormContext

--- src/contexts/FormContext.tsx
+++ src/contexts/FormContext.tsx
@@ -13,11 +13,14 @@ export const FormContext = createContext({
     validationIsActive: () => undefined,
     setValidationFlag: (value) => undefined,
     getErrorByField: (fieldName) => undefined,
-    removeErrorByField: (fieldName) => undefined
+    removeErrorByField: (fieldName) => undefined,
+    removeField: (fieldName: string) => undefined
 });
 
-export const FormProvider = ({
-    children
+
+export const FormProvider = (props: {
+    children: any,
+    currentForm: object
 }) => {
     const [fields, setFields] = useState({});
     const [errors, setErrors] = useState({});
@@ -50,82 +53,95 @@ export const FormProvider = ({
     }
 
     const removeErrorByField = (fieldName) => {
+        console.log('Access error')
         let copyErrors = errors;
         delete copyErrors[fieldName];
         setErrors(copyErrors);
+    }
+
+    const removeField = (fieldName: string) => {
+        let fieldsCopy = fields;
+        delete fieldsCopy[fieldName]
+        setFields(fieldsCopy);
     }
 
     const validateFields = () => {
         let success = true;
         setErrors({});
         let errorsCopy = {};
-        // eslint-disable-next-line
-        Object.keys(fields).map(fieldName => {
-            if (!fields[fieldName].value) {
-                success = false;
-                Object.assign(errorsCopy, { [fieldName]: 'El campo es requerido' })
-            } else {
-                let fieldValue = fields[fieldName].value;
-                // eslint-disable-next-line
-                switch (fields[fieldName].type) {
-                    case 'string':
-                        if (!/^[A-Za-z\s]+$/.test(fieldValue)) {
-                            Object.assign(errorsCopy, { [fieldName]: 'Debe ingresar solo letras' })
+        Object.keys(props.currentForm).map(fieldName => {
+            // reformat fieldname by the value of currentForm[field]
+            fieldName = props.currentForm[fieldName];
+            try {
+                if (!fields[fieldName].value) {
+                    success = false;
+                    Object.assign(errorsCopy, { [fieldName]: 'El campo es requerido' })
+                } else {
+                    let fieldValue = fields[fieldName].value;
+                    // eslint-disable-next-line
+                    switch (fields[fieldName].type) {
+                        case 'string':
+                            if (!/^[A-Za-z\s]+$/.test(fieldValue)) {
+                                Object.assign(errorsCopy, { [fieldName]: 'Debe ingresar solo letras' })
+                                success = false;
+                            }
+                            if (String(fieldValue).length < 2) {
+                                Object.assign(errorsCopy, { [fieldName]: 'Debe contener mas de 2 letras' })
+                                success = false;
+                            }
+                            break;
+                        case 'password':
+                            break;
+                        case 'checkbox':
+                            console.log(fields[fieldName])
+                            if (!fieldValue) {
+                                Object.assign(errorsCopy, { [fieldName]: 'Debe seleccionar el cuadro' })
+                                success = false;
+                            }
+                            break;
+                        case 'select':
+                            if (fieldValue === fields[fieldName].label) {
+                                Object.assign(errorsCopy, { [fieldName]: 'Debe seleccionar una opcion' })
+                                success = false;
+                            }
+                            break;
+                        case 'ci':
+                            // implements the document number validator
+                            if (!validateDoc(fieldValue)) {
+                                Object.assign(errorsCopy, { [fieldName]: 'El documento no es valido!' })
+                                success = false;
+                            }
+                            break;
+                        case 'email':
+                            // eslint-disable-next-line
+                            if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(fieldValue)) {
+                                Object.assign(errorsCopy, { [fieldName]: 'El email no es correco' })
+                                success = false;
+                            }
+                            break;
+                        case 'number':
+                            if (!/^[0-9]+$/.test(fieldValue)) {
+                                Object.assign(errorsCopy, { [fieldName]: 'Debe ingresar solo numeros' })
+                                success = false;
+                            }
+                            break;
+                    }
+                    // check equal field
+                    let equalField = fields[fieldName].equalField;
+                    if (equalField) {
+                        if (fields[fieldName].value !== fields[equalField].value) {
+                            Object.assign(errorsCopy, { [fieldName]: 'Los campos no coinciden' })
+                            Object.assign(errorsCopy, { [equalField]: 'Los campos no coinciden' })
                             success = false;
                         }
-                        if (String(fieldValue).length < 2) {
-                            Object.assign(errorsCopy, { [fieldName]: 'Debe contener mas de 2 letras' })
-                            success = false;
-                        }
-                        break;
-                    case 'password':
-                        break;
-                    case 'checkbox':
-                        console.log(fields[fieldName])
-                        if (!fieldValue) {
-                            Object.assign(errorsCopy, { [fieldName]: 'Debe seleccionar el cuadro' })
-                            success = false;
-                        }
-                        break;
-                    case 'select':
-                        if (fieldValue === fields[fieldName].label) {
-                            Object.assign(errorsCopy, { [fieldName]: 'Debe seleccionar una opcion' })
-                            success = false;
-                        }
-                        break;
-                    case 'ci':
-                        // implements the document number validator
-                        if (!validateDoc(fieldValue)) {
-                            Object.assign(errorsCopy, { [fieldName]: 'El documento no es valido!' })
-                            success = false;
-                        }
-                        break;
-                    case 'email':
-                        // eslint-disable-next-line
-                        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(fieldValue)) {
-                            Object.assign(errorsCopy, { [fieldName]: 'El email no es correco' })
-                            success = false;
-                        }
-                        break;
-                    case 'number':
-                        if (!/^[0-9]+$/.test(fieldValue)) {
-                            Object.assign(errorsCopy, { [fieldName]: 'Debe ingresar solo numeros' })
-                            success = false;
-                        }
-                        break;
-                }
-                // check equal field
-                let equalField = fields[fieldName].equalField;
-                if (equalField) {
-                    if (fields[fieldName].value !== fields[equalField].value) {
-                        Object.assign(errorsCopy, { [fieldName]: 'Los campos no coinciden' })
-                        Object.assign(errorsCopy, { [equalField]: 'Los campos no coinciden' })
-                        success = false;
                     }
                 }
+            } catch (error) {
+                // in this case a field exists in the structure but not in the form
             }
         })
         setErrors(errorsCopy);
+        console.log(errorsCopy)
         return success;
     }
 
@@ -134,6 +150,7 @@ export const FormProvider = ({
         setField,
         getErrorByField,
         removeErrorByField,
+        removeField,
         validationIsActive,
         validateFields,
         setValidationFlag
@@ -141,7 +158,7 @@ export const FormProvider = ({
 
     return (
         <FormContext.Provider value={context}>
-            {children}
+            {props.children}
         </FormContext.Provider>
     );
 };
