3ab0d497e38a8aa3c49f599933d6d596ac355220
damian <damianezetiel1@gmail.com>
2020-12-10 23:02:22 -0300

    create Firebase actions

---
+++ src/actions/firebase/Firebase.actions.tsx
@@ -0,0 +1,19 @@
+import db from "../../config/firebase";
+
+export default class FirebaseActions {
+
+    getQuery = async (collections: string[], docId: string) => {
+        const resRef = await db
+            .collection(collections[0])
+            .doc(docId)
+            .collection(collections[1]);
+
+        const result = await resRef
+            .get()
+            .then((snapshot) => {
+                return snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            })
+            .catch((err) => console.error(err));
+        return result;
+    }
+}
