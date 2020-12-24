ae4ccd86d4bce5d1394e5c58462530d28fcda584
damian <damianezetiel1@gmail.com>
2020-12-23 21:46:18 -0300

    create row table component

---
+++ src/components/custom-table/row-table/index.tsx
@@ -0,0 +1,57 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import { MdEdit } from 'react-icons/md';
+import { Text } from '../../decorators/text';
+import { Button } from '../../inputs/button';
+import { HEADER } from '../table.type';
+
+export const RowTable = (props: {
+    item: any
+    selected: boolean
+    headers: HEADER[]
+    onSelect: () => void
+    onEdit: () => void
+}) => {
+
+    const getColumnValue = (header) => {
+        switch (header) {
+            case 'status': // TODO not used yet 
+                return (
+                    <Button
+                        style="outlined"
+                        className="state-btn"
+                        label="status"
+                    />
+                )
+            case 'actions':
+                return (
+                    <Button
+                        style="text"
+                        icon={<MdEdit />}
+                        onClick={() => { props.onEdit() }}
+                    />
+                )
+            default:
+                return <Text type="text">{props.item[header]}</Text>
+        }
+    }
+
+    return (
+        <div className={`row ${props.selected && 'selected-row'}`}
+            onClick={() => {
+                props.onSelect();
+            }}
+        >
+            <Grid container xl={12} spacing={3}>
+                {props.headers.map((header, i) => {
+                    return (
+                        <Grid item xs key={i} className={`header_${i}`}>
+                            {getColumnValue(header.value)}
+                        </Grid>
+                    )
+                }
+                )}
+            </Grid>
+        </div>
+    )
+}
