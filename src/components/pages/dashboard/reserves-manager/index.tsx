eb074149c403f8a833baf86514ba120cc0d6d04f
damian <damianezetiel1@gmail.com>
2020-12-23 21:56:40 -0300

    comment code for refactor in Reserve Manager

--- src/components/pages/dashboard/reserves-manager/index.tsx
+++ src/components/pages/dashboard/reserves-manager/index.tsx
@@ -2,24 +2,31 @@ import React, { useContext, useEffect, useState } from 'react';
 import ReserveActions from '../../../../actions/Reserve.actions';
 import { ButtonContext } from '../../../../contexts/ButtonsContext';
 import { CustomTable } from '../../../custom-table/custom-table';
-import { headerOrder } from '../../../../data/dashboard';
+import { headerMobileOrder, headerOrder } from '../../../../data/dashboard';
 import { IReserve } from '../../../../types/Reserve.type';
 import { ManagerDialog } from './manager-dialog';
-import { Card } from '../../../containers/card';
 import moment from "moment";
 import { Text } from '../../../decorators/text';
+import { Button } from '../../../inputs/button';
+import { FaCalendarCheck } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import { ConfirmDialog } from '../../../dialogs/confirm-dialog';
 import './reserve-manager.scss';
 
 
 export const ReserveManager = () => {
-    const [reserves, setReserve] = useState([]);
+
+    const [reserves, setReserves] = useState([]);
+
     const [selectedReserve, setSelectedReserve] = useState(undefined);
     const [showReserveDialog, setShowReserveDialog] = useState(false);
-
+    const [showFinalizeDialog, setFinalizeDialog] = useState(false);
+    const [showCancelDialog, setCancelDialog] = useState(false);
 
     const reserveActions: ReserveActions = new ReserveActions();
-    const mobileHeaders = [headerOrder[1], headerOrder[2], headerOrder[3]];
 
+    // Mobile headers
+    const mobileHeaders = headerMobileOrder;
 
     const {
         // @ts-ignore
@@ -29,91 +36,116 @@ export const ReserveManager = () => {
 
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchGetReserves = async () => {
             await getReserves();
         };
-        // check user is admin
-        // !userIsAdmin() ? (document.location.href = "/") : (
-        false ? (document.location.href = "/") : (
-            // get reserves
-            fetchData()
-        )
+        /* check user is admin
+            // !userIsAdmin() ? (document.location.href = "/") : (
+        */
+        fetchGetReserves()
     }, [])
-
 
     const getReserves = async () => {
         setDisabledButton(true);
         const reserves: any[] = await reserveActions.getAll();
-        setDisabledButton(false);
         if (reserves) {
-            // formatting date
+            /* formatting date */
             reserves.map((reserve: IReserve) => {
                 reserve.startTimeFront = moment(reserve.startTime)
                     .format("DD/MM/YYYY hh:mm:ss")
                     .substr(0, 16);
             })
-            setReserve(reserves);
+            setReserves(reserves);
         }
+        setDisabledButton(false);
     }
 
-
-    const showSelectedReserve = (reserve: any) => {
+    const showEditReserve = (reserve: any) => {
         setSelectedReserve(reserve);
         setShowReserveDialog(true);
     }
-
-
-    const onCancelled = () => {
-        // remove reserve
-        reserves.splice(reserves.indexOf(selectedReserve), 1);
+    const showActionsReserve = (reserve: any) => {
+        setSelectedReserve(reserve);
+        // setShowReserveDialog(true);
     }
-
-    const onFinalized = () => {
-        // remove reserve
-        reserves.splice(reserves.indexOf(selectedReserve), 1);
-    }
-
-    const onUpdated = (updated) => {
-        async (updated) => {
-            /* Forma no recomendada pero para salir del paso */
-            setReserve([]);
-            await getReserves();
-            setShowReserveDialog(false);
-        }
-    }
-
 
     return (
-        <Card
-            title="Administracion de Reservas"
-            className="tool-card"
-        >
-
-            {reserves.length !== 0 ? (
-                <CustomTable
-                    noItemsMessage="No tiene reservas creadas"
-                    noSearchMessage="No se encontraron coincidencias"
-                    items={reserves}
-                    headers={headerOrder}
-                    mobileHeaders={mobileHeaders}
-                    sortColumnByOtherHeader={{
-                        headerToAction: "startTimeFront",
-                        headerToSort: "reserveId",
-                    }}
-                    onSelectRow={showSelectedReserve}
-                />
-            ) : <Text type="text" className="no-reserves">No hay reservas creadas</Text>}
-
-            {selectedReserve && showReserveDialog && (
-                <ManagerDialog
-                    reserve={selectedReserve}
-                    onUpdated={((updated) => onUpdated(updated))}
-                    onCancelled={() => onCancelled()}
-                    onFinalized={() => onFinalized()}
-                    onClose={setShowReserveDialog}
-                />
-            )}
-
-        </Card>
+        <div>
+            <CustomTable
+                items={reserves}
+                headers={headerOrder}
+                onSelectRow={showActionsReserve}
+                onEditItem={showEditReserve}
+                sortColumnByHeader={{
+                    headerToAction: "startTimeFront",
+                    headerToSort: "reserveId",
+                }}
+                footerItems={[]}
+            />
+        </div>
     )
 }
+
+
+{/* {reserves.length ? (
+                
+            ) : (
+                    <div className="no-items">
+                        <Text type="subtitle">No hay reservas creadas</Text>
+                    </div>
+                )} */}
+
+                 // <Button
+                    //     style="normal"
+                    //     label="Finalizar"
+                    //     icon={<FaCalendarCheck />}
+                    //     onClick={() => { setFinalizeDialog(true) }} />,
+                    // <Button
+                    //     style="normal"
+                    //     label="Cancelar"
+                    //     icon={<AiOutlineClose />}
+                    //     onClick={() => { setCancelDialog(true) }} />
+
+
+                    // const onCancelled = () => {
+                    //     // remove reserve
+                    //     reserves.splice(reserves.indexOf(selectedReserve), 1);
+                    // }
+                    // const onFinalized = () => {
+                    //     // remove reserve
+                    //     reserves.splice(reserves.indexOf(selectedReserve), 1);
+                    // }
+                    // const onUpdated = (updated) => {
+                    //     async (updated) => {
+                    //         /* Forma no recomendada pero para salir del paso */
+                    //         setReserves([]);
+                    //         await getReserves();
+                    //         setShowReserveDialog(false);
+                    //     }
+                    // }
+                    // /* FINALIZE RESERVE */
+                    // const finalize = async () => {
+                    //     setDisabledButton(true);
+                    //     let response = await reserveActions.doneReserve(
+                    //         selectedReserve.barberOrHairdresserId,
+                    //         selectedReserve.reserveId
+                    //     );
+                    //     if (response) {
+                    //         // props.onFinalized();
+                    //         // props.onClose();
+                    //     }
+                    //     setDisabledButton(false);
+                    // }
+                    // /* CANCEL RESERVE */
+                    // const cancel = async () => {
+                    //     setDisabledButton(true);
+                    //     let response = await reserveActions.cancel(
+                    //         selectedReserve.clientId,
+                    //         selectedReserve.reserveId
+                    //     )
+                    //     if (response) {
+                    //         // props.onCancelled();
+                    //         // props.onClose();
+                    //     }
+                    //     setDisabledButton(false);
+                    // }
