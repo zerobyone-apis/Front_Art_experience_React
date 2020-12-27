ef28cc8e8e11e5daacfb0d388831da364db103ac
damian <damianezetiel1@gmail.com>
2020-12-27 16:19:18 -0300

    ReservesManager - remove code

--- src/components/pages/dashboard/reserves-manager/index.tsx
+++ src/components/pages/dashboard/reserves-manager/index.tsx
@@ -4,13 +4,7 @@ import { ButtonContext } from '../../../../contexts/ButtonsContext';
 import { CustomTable } from '../../../custom-table/custom-table';
 import { headerMobileOrder, headerOrder } from '../../../../data/dashboard';
 import { IReserve } from '../../../../types/Reserve.type';
-import { ManagerDialog } from './manager-dialog';
 import moment from "moment";
-import { Text } from '../../../decorators/text';
-import { Button } from '../../../inputs/button';
-import { FaCalendarCheck } from 'react-icons/fa';
-import { AiOutlineClose } from 'react-icons/ai';
-import { ConfirmDialog } from '../../../dialogs/confirm-dialog';
 import './reserve-manager.scss';
 
 
@@ -85,67 +79,3 @@ export const ReserveManager = () => {
         </div>
     )
 }
-
-
-{/* {reserves.length ? (
-                
-            ) : (
-                    <div className="no-items">
-                        <Text type="subtitle">No hay reservas creadas</Text>
-                    </div>
-                )} */}
-
-                 // <Button
-                    //     style="normal"
-                    //     label="Finalizar"
-                    //     icon={<FaCalendarCheck />}
-                    //     onClick={() => { setFinalizeDialog(true) }} />,
-                    // <Button
-                    //     style="normal"
-                    //     label="Cancelar"
-                    //     icon={<AiOutlineClose />}
-                    //     onClick={() => { setCancelDialog(true) }} />
-
-
-                    // const onCancelled = () => {
-                    //     // remove reserve
-                    //     reserves.splice(reserves.indexOf(selectedReserve), 1);
-                    // }
-                    // const onFinalized = () => {
-                    //     // remove reserve
-                    //     reserves.splice(reserves.indexOf(selectedReserve), 1);
-                    // }
-                    // const onUpdated = (updated) => {
-                    //     async (updated) => {
-                    //         /* Forma no recomendada pero para salir del paso */
-                    //         setReserves([]);
-                    //         await getReserves();
-                    //         setShowReserveDialog(false);
-                    //     }
-                    // }
-                    // /* FINALIZE RESERVE */
-                    // const finalize = async () => {
-                    //     setDisabledButton(true);
-                    //     let response = await reserveActions.doneReserve(
-                    //         selectedReserve.barberOrHairdresserId,
-                    //         selectedReserve.reserveId
-                    //     );
-                    //     if (response) {
-                    //         // props.onFinalized();
-                    //         // props.onClose();
-                    //     }
-                    //     setDisabledButton(false);
-                    // }
-                    // /* CANCEL RESERVE */
-                    // const cancel = async () => {
-                    //     setDisabledButton(true);
-                    //     let response = await reserveActions.cancel(
-                    //         selectedReserve.clientId,
-                    //         selectedReserve.reserveId
-                    //     )
-                    //     if (response) {
-                    //         // props.onCancelled();
-                    //         // props.onClose();
-                    //     }
-                    //     setDisabledButton(false);
-                    // }
