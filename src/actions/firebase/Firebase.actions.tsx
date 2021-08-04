04c687a25d8f46a9225d1a979bd11a37f1ad8102
DamianRz <damian.rodriguez01@globant.com>
2021-08-04 10:08:51 -0300

    refactor firebase action

--- src/actions/firebase/Firebase.actions.tsx
+++ src/actions/firebase/Firebase.actions.tsx
@@ -14,21 +14,3 @@ export async function getQuery(collections: string[], docId: string) {
         .catch((err) => console.error(err));
     return result;
 }
-
-// export default class FirebaseActions {
-
-//     getQuery = async (collections: string[], docId: string) => {
-//         const resRef = await db
-//             .collection(collections[0])
-//             .doc(docId)
-//             .collection(collections[1]);
-
-//         const result = await resRef
-//             .get()
-//             .then((snapshot) => {
-//                 return snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-//             })
-//             .catch((err) => console.error(err));
-//         return result;
-//     }
-// }
