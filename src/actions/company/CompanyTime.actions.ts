15aa566d16516b3939536edb83f24fed65dee2c0
DamianRz <damian.rodriguez01@globant.com>
2021-08-04 10:08:17 -0300

    refactor company action using constants

--- src/actions/company/CompanyTime.actions.ts
+++ src/actions/company/CompanyTime.actions.ts
@@ -1,95 +1,47 @@
-import IntegrationBackend from '../../utils/IntegrationBackend';
-import { RESERVE_DATES_ROUTE, GET_ENDPOIT } from '../../types/Routes.type';
+export const COMPANY_TIMES = [
+  '10:00',
+  '10:40',
+  '11:20',
+  '12:00',
+  '12:40',
+  '13:20',
+  '14:00',
+  '14:40',
+  '15:20',
+  '16:00',
+  '16:40',
+  '17:20',
+  '18:00',
+  '18:40',
+];
 
-export default class CompanyHours {
-  private backend: IntegrationBackend = new IntegrationBackend();
-
-  /* Burn data */
-  async getCompanyTimes() {
-    return [
-      '10:00',
-      '10:40',
-      '11:20',
-      '12:00',
-      '12:40',
-      '13:20',
-      '14:00',
-      '14:40',
-      '15:20',
-      '16:00',
-      '16:40',
-      '17:20',
-      '18:00',
-      '18:40',
-    ];
-  }
-
-  /* Chritmas update times */
-  async getExceptionTimes() {
-    return [
-      '08:00',
-      '08:30',
-      '09:00',
-      '09:30',
-      '10:00',
-      '10:30',
-      '11:00',
-      '11:30',
-      '12:00',
-      '12:30',
-      '13:00',
-      '13:30',
-      '14:00',
-      '14:30',
-      '15:00',
-      '15:30',
-      '16:00',
-      '16:30',
-      '17:00',
-      '17:30',
-      '18:00',
-      '18:30',
-      '19:00',
-      '19:30',
-      '20:00',
-      '20:30',
-      '21:00',
-    ];
-  }
-
-  /* Deprecated */
-  async getReserveTimes(barberId: number) {
-    try {
-      const responseDates: any = await this.backend.send(
-        GET_ENDPOIT,
-        undefined,
-        `${RESERVE_DATES_ROUTE}/${barberId}`
-      );
-      if (responseDates.status != 200) {
-        throw Error(`Error Status code: , ${responseDates.status}`);
-      }
-      // Formatting data
-      let formattingData = [];
-      responseDates.data.forEach((item: { date: string; hours: string[] }) => {
-        formattingData.push({ date: item.date, hours: [...item.hours] });
-      });
-      return responseDates.data;
-    } catch (error) {
-      console.error(`Error: Obteniendo fecha de reservas -> ${error}`);
-      return [];
-    }
-  }
-
-  /* For tests */
-  async getReserveTimesTest(barberId: number) {
-    return [
-      { date: '2020-08-26', hours: ['16:00', '16:30', '17:00'] },
-      { date: '2020-08-27', hours: ['14:30', '15:00', '15:30', '17:00'] },
-      { date: '2020-08-28', hours: ['14:00', '14:30', '15:00', '15:30'] },
-      {
-        date: '2020-08-29',
-        hours: ['14:00', '15:30', '16:00', '16:30', '17:00'],
-      },
-    ];
-  }
-}
+// esto no se usa por el momento 
+export const EXCEPTION_TIMES = [
+  '08:00',
+  '08:30',
+  '09:00',
+  '09:30',
+  '10:00',
+  '10:30',
+  '11:00',
+  '11:30',
+  '12:00',
+  '12:30',
+  '13:00',
+  '13:30',
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+  '18:00',
+  '18:30',
+  '19:00',
+  '19:30',
+  '20:00',
+  '20:30',
+  '21:00',
+];
