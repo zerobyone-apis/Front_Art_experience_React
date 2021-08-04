63eb55bdf72db1d39b54a587a399a79d00069627
DamianRz <damian.rodriguez01@globant.com>
2021-08-04 10:10:39 -0300

    refactor in useTimeSelector

--- src/components/dialogs/reserve-dialog/time-step/hooks/useTimeSelector.tsx
+++ src/components/dialogs/reserve-dialog/time-step/hooks/useTimeSelector.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import {getQuery} from "../../../../../actions/firebase/Firebase.actions";
 
 const useTimeSelector = () => {
-    const [reservesList, setReservesList] = useState([])
+    // const [reservesList, setReservesList] = useState([])
 
     const filterTimes = (firebaseResponse: any) => {
         let formattedDates: { 
@@ -19,20 +19,33 @@ const useTimeSelector = () => {
             times: item.times,
           });
         });
-    
         return formattedDates
     }
     
     const loadReserveTimes = async (barberName: string) => {
-        console.log("CCCC")
-        const formattedBarberName = barberName.toLowerCase().replace("/' '/g", ".")
-        const response = await getQuery(["reservas", "day_reserves"], formattedBarberName) 
-        setReservesList(filterTimes(response))
+      // obtiene todas las reservas 
+      const formattedBarberName = barberName.toLowerCase().replace("/' '/g", ".")
+      const response = await getQuery(["reservas", "day_reserves"], formattedBarberName)
+      return filterTimes(response)
+    }
+
+    const getBusyHours = (reservesList, selectedDate) => {
+      let listBusyHours = [];
+      // map hours by reserves
+      reservesList.map((reserve: { date: string; times: string[] }) => {
+        if (reserve.date === selectedDate) {
+          reserve.times.map((reserveHour: string) => {
+            listBusyHours.push(reserveHour.substr(0, 5));
+          });
+        }
+      });
+      return listBusyHours;
     }
 
     return {
-        reservesList: reservesList,
-        loadReserveTimes: loadReserveTimes
+        // reservesList: reservesList,
+        loadReserveTimes: loadReserveTimes,
+        getBusyHours: getBusyHours
     }
 }
 export default useTimeSelector;
