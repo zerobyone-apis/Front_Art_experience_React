f1f19a9aee379ee926c7d7d15a1aae6c75c8b48c
Maximiliano Olivero <37275050+Max-Oliver@users.noreply.github.com>
2021-08-30 18:54:25 -0300

    Update IntegrationBackend.ts

--- src/utils/IntegrationBackend.ts
+++ src/utils/IntegrationBackend.ts
@@ -3,7 +3,22 @@ export default class IntegrationBackend {
   //private baseUrl = 'http://artexperienceproductioninstance-env.eba-iimpqhxr.us-east-2.elasticbeanstalk.com';
   
   /* With SSL Certificate Domain to router new Backend on AWS ELB */
-  private baseUrl = 'https://back-artexperience.xyz';
+  /* Todo:
+        Temporalmente caido, estamos trabajando para resolverlo
+        Problemas: AWS Certificate Manager, Route53, LoadBalancer HTTPS
+        caus: Basicamente no funciona la escucha por HTTPS, esto se debe a que el load balancer no lo entiende bien, 
+              mala configuracion o algo con ACM cerificate Manager el tema es que tenemos que resolver eso para que se conecte back y front.
+  
+        Nota: Los servidores del dominio de GoDaddy estan en Route53 por lo que todo el redireccionamiento esta en AWS. igual probar diferentes cosas.
+  
+  private baseUrl = 'https://back-artexperience.xyz'; 
+  */
+  
+  /*    Heroku apuntando a nuestra base de datos de AWS 
+        Plan B, Pagar el servicio de heroku mensual para que nos hostee la app mientras resolvemos o entendemos mejor lo de AWS.
+        Plan de 7 USD por mes
+  */
+  private baseUrl = 'https://back-artexperience-aws-rds.herokuapp.com';
   
   private axios = require('axios');
 
